6d18f2654d02c99795cef10477c1b26d64b6f7c7
vctrferreira <victtorferreiralima@gmail.com>
2018-01-10 11:11:34 -0300

    Change spaces to tabs and start structure card component and list flags

--- bothub/assets/js/components/BotCard.vue
+++ bothub/assets/js/components/BotCard.vue
@@ -1,29 +1,63 @@
 <template>
-  <div class="bot-card">
-    {{card.bot_name}}
-  </div>
+	<div class="bot-card">
+		<div class="up-down-card-column"></div>
+		<div class="image-card-column"></div>
+		<div class="body-card-column">
+			<h3>{{card.bot_name}}</h3>
+			<span>{{card.updated_at}}</span>
+			<list-flags :flags="card.languages"></list-flags>
+			<hr>
+			<p>
+				{{card.description}}
+			</p>
+		</div>
+	</div>
 </template>
+
 <script>
+import ListFlags from "./ListFlags.vue"
 export default {
-  name: 'card',
-  props: {
-    card: {
-      type: Object,
-      required: true
-    }
-  },
-  mounted() {
-  },
-  methods:{
-  }
+	name: 'card',
+	props: {
+		card: {
+			type: Object,
+			required: true
+		}
+	},
+	mounted() {
+	},
+	methods:{
+	},
+	components:{
+		ListFlags
+	}
 }
 </script>
 <style>
-  .bot-card{
-      width: 80%;
-      background: red;
-      border-radius: 25px;
-      padding: 25px;
-      height: 160px;
-  }
+	.bot-card {
+		margin: 0 auto;
+		width: 80%;
+		background: red;
+		border-radius: 25px;
+		padding: 25px;
+		height: 160px;
+	}
+	.up-down-card-column {
+		width: 10%;
+		height: 100%;
+		background: black;
+		float: left;
+	}
+	.image-card-column {
+		width: 20%;
+		height: 100%;
+		background: yellow;
+		float: left;
+	}
+	.body-card-column {
+		width: 70%;
+		height: 100%;
+		background: green;
+		float: left;
+	}
 </style>
---
+++ bothub/assets/js/components/ListFlags.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <span :key="i" v-for="(flag, i) in flags">
+            {{flag}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        flags: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
