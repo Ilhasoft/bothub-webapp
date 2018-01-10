fe8183c8b221503f1b8707c410ed21935e11ab1f
vctrferreira <victtorferreiralima@gmail.com>
2018-01-10 16:05:12 -0300

    Return folder path and bulk data compontents

--- bothub/public/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='public/index.html'), name='index'),
-]
