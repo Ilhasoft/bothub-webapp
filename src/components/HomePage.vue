a7aed5fcef26d1da96b1a6fba54f45a9a37e1505
Mac Giovanni <maclevison@gmail.com>
2018-01-17 18:03:39 -0300

    Update Package Json

---
+++ src/components/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ mensagem }} <span>{{ subtitulo }}</span></h1>
+    <card-bot></card-bot>
+  </div>
+</template>
+
+<script>
+import CardBot from './CardBot.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    CardBot
+  },
+  data () {
+    return {
+      mensagem: 'Título da página',
+      subtitulo: 'Aqui entra o subtitulo'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  h1 {
+    width: 80%;
+    position: relative;
+    margin: 0 auto;
+    font-weight: normal;
+    background-color: #42b983;
+    span {
+      color: #FFF;
+      display: block;
+      font-size: 16px;
+    }
+  }
+</style>
