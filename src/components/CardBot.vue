8ba3c5dc634bb4ca0ee81bf54dc08a1f3b499595
Mac Giovanni <maclevison@gmail.com>
2018-01-17 18:03:50 -0300

    New Project

---
+++ src/components/CardBot.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-bot">
+    <span class="card-bot__title">Card Bot Title</span>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+  div {
+    width: 80%;
+    position: relative;
+    margin: 0 auto;
+    font-weight: normal;
+    background-color: #CCC;
+    span {
+      color: #FFF;
+      display: block;
+      font-size: 16px;
+    }
+  }
+</style>
