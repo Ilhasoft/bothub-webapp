e6f9dfa193b4adfb7b8191f435288fb2a6a1d64a
Mac Giovanni <maclevison@gmail.com>
2018-01-24 14:57:42 -0300

    Add Badges

--- src/components/CardBot.vue
+++ src/components/CardBot.vue
@@ -1,24 +1,59 @@
 <template>
   <div class="card-bot">
-    <span class="card-bot__title">Card Bot Title</span>
+    <div class="card-bot__image">
+      <img src="../assets/images/avatar.png" alt="">
+    </div>
+    <div class="card-bot__content">
+      <div class="card-bot__info">
+        <span class="card-bot__title">{{ bot_title }}</span>
+        <span class="card-bot__author">{{ bot_author }}</span>
+      </div>
+      <div class="card-bot__text">
+        {{ bot_text }}
+      </div>
+    </div>
+    <div class="card-bot--tags">
+      <span class="badge">chers</span>
+      <span class="badge">bug</span>
+      <span class="badge">precaition</span>
+      <span class="badge">virus</span>
+      <span class="badge">disease</span>
+      <span class="badge">symptoms</span>
+      <span class="badge">...</span>
+    </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'CardBot',
+  data () {
+    return {
+      bot_title: 'Bot-ZicaVirus',
+      bot_author: 'david_gazques',
+      bot_text: 'ASwimming hundreds of feet beneath the ocean’s surface in many parts of the world are prolific architects'
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
-  div {
+  .card-bot {
+    display: flex;
+    background-color: #ccc;
+    border-radius: 20px;
+    padding: 20px;
+  }
+  .card-bot__image {
+    width: 134px;
+    img {
+      width: 100%;
+    }
+  }
+  .container {
     width: 80%;
     position: relative;
     margin: 0 auto;
     font-weight: normal;
-    background-color: #CCC;
-    span {
-      color: #FFF;
-      display: block;
-      font-size: 16px;
-    }
   }
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,38 +1,54 @@
 <template>
-  <div>
+  <div class="container">
     <h1>{{ mensagem }} <span>{{ subtitulo }}</span></h1>
+    <div class="search">
+      <search></search>
+    </div>
+    <div class="card-list">
+      <h3 class="card-list__title">
+        Best bots of the month
+      </h3>
+    </div>
+
+    <div class="badge badge--danger">Danger</div>
     <card-bot></card-bot>
   </div>
 </template>
 
 <script>
 import CardBot from './CardBot.vue'
+import Search from './Search.vue'
 
 export default {
   name: 'HomePage',
   components: {
-    CardBot
+    CardBot,
+    Search
   },
   data () {
     return {
-      mensagem: 'Título da página',
-      subtitulo: 'Aqui entra o subtitulo'
+      mensagem: 'Robot Society',
+      subtitulo: 'TRAIN YOURS'
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+  @import '../assets/scss/_variables';
   h1 {
-    width: 80%;
+    font-family: $font-secondary;
+    width: 100%;
     position: relative;
     margin: 0 auto;
     font-weight: normal;
     background-color: #42b983;
+    text-align: center;
     span {
       color: #FFF;
       display: block;
       font-size: 16px;
+      font-weight: 300;
     }
   }
 </style>
