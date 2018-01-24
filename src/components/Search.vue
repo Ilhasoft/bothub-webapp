df3d4f015f0507586cee7bff8a1e0bbc09442c6d
Mac Giovanni <maclevison@gmail.com>
2018-01-24 16:11:09 -0300

    Add Seach

---
+++ src/components/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="the world of robots is here">
+    <input type="submit" value="Ok">
+    <button>Ok</button>
+    <a href="#" class="button">Ok</a>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+  .search {
+  }
+</style>
