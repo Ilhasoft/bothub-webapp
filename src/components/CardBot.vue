5d2fdcc97b7c10a3a3a91ab6184e406e6ac149cb
Mac Giovanni <maclevison@gmail.com>
2018-01-24 16:08:06 -0300

    Custom card bot

--- src/components/CardBot.vue
+++ src/components/CardBot.vue
@@ -4,22 +4,36 @@
       <img src="../assets/images/avatar.png" alt="">
     </div>
     <div class="card-bot__content">
-      <div class="card-bot__info">
+      <div class="card-bot__header">
         <span class="card-bot__title">{{ bot_title }}</span>
         <span class="card-bot__author">{{ bot_author }}</span>
       </div>
-      <div class="card-bot__text">
+      <div class="card-bot__info">
+        <div class="card-bot__updated">
+          Updated 2 weeks ago by {{ bot_author_name }}
+        </div>
+        <ul class="card-bot__flags">
+          <li class="flags-item"><img src="../assets/images/flags/EN.png" alt="Flag ES"></li>
+          <li class="flags-item"><img src="../assets/images/flags/ES.png" alt="Flag ES"></li>
+          <li class="flags-item"><img src="../assets/images/flags/PT.png" alt="Flag PT"></li>
+          <li class="flags-item"><img src="../assets/images/flags/IT.png" alt="Flag IT"></li>
+          <li class="flags-item"><img src="../assets/images/flags/NL.png" alt="Flag NL"></li>
+          <li class="flags-item"><img src="../assets/images/flags/FR.png" alt="Flag FR"></li>
+          <li class="flags-item"><img src="../assets/images/flags/DE.png" alt="Flag DE"></li>
+        </ul>
+      </div>
+      <div class="card-bot__excerpt">
         {{ bot_text }}
       </div>
-    </div>
-    <div class="card-bot--tags">
-      <span class="badge">chers</span>
-      <span class="badge">bug</span>
-      <span class="badge">precaition</span>
-      <span class="badge">virus</span>
-      <span class="badge">disease</span>
-      <span class="badge">symptoms</span>
-      <span class="badge">...</span>
+      <div class="card-bot__badge">
+        <span class="badge">cheers</span>
+        <span class="badge">bug</span>
+        <span class="badge">precaution</span>
+        <span class="badge">virus</span>
+        <span class="badge">disease</span>
+        <span class="badge">symptoms</span>
+        <span class="badge">...</span>
+      </div>
     </div>
   </div>
 </template>
@@ -31,29 +45,90 @@ export default {
     return {
       bot_title: 'Bot-ZicaVirus',
       bot_author: 'david_gazques',
-      bot_text: 'ASwimming hundreds of feet beneath the ocean’s surface in many parts of the world are prolific architects'
+      bot_author_name: 'David Gazques',
+      bot_text: 'ASwimming hundreds of feet beneath the ocean’s surface in many parts of the world are prolific architects. calledlarvaceans. ASwimming hundreds of feet beneath the ocean’s surface in many parts of the architects called.'
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+  @import "../assets/scss/_colors";
   .card-bot {
     display: flex;
-    background-color: #ccc;
+    background-color: #FFF;
     border-radius: 20px;
     padding: 20px;
+    flex-wrap: wrap;
+    position: relative;
+    transition: all .2s ease-in-out;
+    align-items: center;
+    &:hover {
+      box-shadow: 0 0 20px 5px rgba($dark-green,.2);
+    }
+  }
+  .card-bot__title,
+  .card-bot__author {
+    font-weight: 500;
+    font-size: 18px;
+    display: inline-block;
+    margin-bottom: 7px;
+  }
+  .card-bot__author {
+    position: relative;
+    padding-left: 8px;
+    &::before {
+      content: '/';
+      display: block;
+      width: 8px;
+      height: 20px;
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  }
+  .card-bot__info {
+    display: flex;
+    align-items: center;
+  }
+  .card-bot__updated {
+    font-size: 10px;
+    color: $dark-grey;
+    margin-right: 10px;
+  }
+  .card-bot__flags {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    .flags-item {
+      width: 15px;
+      margin: 0 3px;
+      img {
+        width: 100%;
+      }
+    }
   }
   .card-bot__image {
-    width: 134px;
+    max-width: 134px;
+    height: 100%;
+    margin-right: 30px;
     img {
       width: 100%;
     }
   }
-  .container {
-    width: 80%;
-    position: relative;
-    margin: 0 auto;
-    font-weight: normal;
+  .card-bot__excerpt {
+    margin: 12px 0;
+    font-size: 14px;
+    line-height: 18px;
+  }
+  .card-bot__content {
+    width: calc(100% - 164px);
+  }
+  .card-bot__badge {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
   }
 </style>
