1553a2a69b6097da48f82ee6f505408481c98bbf
Mac Giovanni <maclevison@gmail.com>
2018-02-02 10:45:07 -0300

    Card author base

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,13 +6,75 @@
       <search></search>
     </div>
     <div class="card-list">
-      <h3 class="card-list__title">
+      <h3 class="section-title">
         Best bots of the month
       </h3>
     </div>
 
     <div class="badge badge--danger">Danger</div>
     <card-bot></card-bot>
+
+  <section class="top-authors">
+    <h3 class="section-title">
+      Top Authors
+    </h3>
+    <div class="authors-list">
+      <div class="card-author">
+        <div class="card-author__image">
+          <img src="../assets/images/man.png" alt="">
+        </div>
+        <div class="card-author__info">
+          <div class="author__name">Matthew Russell</div>
+          <div class="author__info">
+          <span class="author__location">
+            Granada, Andalusia, Spain
+          </span>
+            <span class="author__joined">
+            Joined 2 years ago · last seen 9 days ago
+          </span>
+          </div>
+          <a href="#" class="auhor__url">http://davidgasquez.buthub.api/</a>
+          <span class="author__bot-counts">60 bots</span>
+        </div>
+      </div>
+      <div class="card-author">
+        <div class="card-author__image">
+          <img src="../assets/images/man.png" alt="">
+        </div>
+        <div class="card-author__info">
+          <div class="author__name">Matthew Russell</div>
+          <div class="author__info">
+          <span class="author__location">
+            Granada, Andalusia, Spain
+          </span>
+            <span class="author__joined">
+            Joined 2 years ago · last seen 9 days ago
+          </span>
+          </div>
+          <a href="#" class="auhor__url">http://davidgasquez.buthub.api/</a>
+          <span class="author__bot-counts">60 bots</span>
+        </div>
+      </div>
+      <div class="card-author">
+        <div class="card-author__image">
+          <img src="../assets/images/man.png" alt="">
+        </div>
+        <div class="card-author__info">
+          <div class="author__name">Matthew Russell</div>
+          <div class="author__info">
+          <span class="author__location">
+            Granada, Andalusia, Spain
+          </span>
+            <span class="author__joined">
+            Joined 2 years ago · last seen 9 days ago
+          </span>
+          </div>
+          <a href="#" class="auhor__url">http://davidgasquez.buthub.api/</a>
+          <span class="author__bot-counts">60 bots</span>
+        </div>
+      </div>
+    </div>
+  </section>
   </div>
 </template>
 
@@ -53,5 +115,29 @@ export default {
       font-size: 16px;
       font-weight: 300;
     }
+  }
+/*Card Author */
+  .authors-list {
+    display: flex;
+  }
+  .card-author {
+    background-color: #fff;
+    border-radius: 10px;
+    width: 33%;
+    padding: 20px;
+    display: flex;
+    margin: 15px;
+  }
+  .card-author__image {
+    width: 100px;
+    margin-right: 20px;
+    overflow: hidden;
+    border-radius: 10px;
+    img {
+      width: 100%;
+    }
+  }
+  .card-author__info {
+    width: calc(100% - 120px);
   }
 </style>
