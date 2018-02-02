efffed4b1367e965e02512ffe17794b8dd6e6111
Mac Giovanni <maclevison@gmail.com>
2018-02-02 16:42:59 -0300

    Card Author update

---
+++ src/components/CardAuthor.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="card-author">
+    <div class="card-author__image">
+      <img src="../assets/images/man.png" alt="">
+    </div>
+    <div class="card-author__info">
+      <div class="author__name">{{ author_name }}</div>
+      <div class="author__info">
+      <span class="author__location">
+        {{ author_location }}
+      </span>
+        <span class="author__joined">
+        Joined {{ author_joined }} · last seen {{ author_last_seen }}
+      </span>
+      </div>
+      <a href="#" class="author__url">{{ author_url }}</a>
+      <span class="author__bot-counts">{{ author_bot_counts }} bots</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardAuthor',
+  data () {
+    return {
+      author_name: 'Matthew Russell',
+      author_location: 'Granada, Andalusia, Spain',
+      author_joined: '2 years ago',
+      author_last_seen: '9 days ago',
+      author_url: 'http://davidgasquez.buthub.api/',
+      author_bot_counts: '60'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import '../assets/scss/_colors';
+
+  .authors-list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .card-author {
+    background-color: #fff;
+    border-radius: 10px;
+    width: calc(33.33% - 30px);
+    padding: 20px;
+    display: flex;
+    margin: 15px;
+
+    @media (max-width: 992px) {
+      width: calc(50% - 30px);
+    }
+
+    @media (max-width: 768px) {
+      width: 100%;
+      margin: 10px 0;
+    }
+  }
+  .card-author__image {
+    width: 100px;
+    margin-right: 20px;
+    overflow: hidden;
+    border-radius: 10px;
+    img {
+      width: 100%;
+    }
+  }
+  .card-author__info {
+    width: calc(100% - 120px);
+    font-size: 10px;
+    line-height: 16px;
+  }
+  .author__name {
+    font-size: 14px;
+    font-weight: 500;
+    margin-bottom: 10px;
+  }
+  .author__location {
+    display: block;
+  }
+  .author__joined {
+    display: block;
+  }
+  .author__url {
+    display: block;
+    color: #000;
+    text-decoration: none;
+    &:hover {
+      color: $primary-color;
+    }
+  }
+  .author__bot-counts {
+    display: block;
+    margin-top: 10px;
+  }
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,85 +1,36 @@
 <template>
   <div class="container">
-    <!--<topbar></topbar>-->
-    <h1>{{ mensagem }} <span>{{ subtitulo }}</span></h1>
+    <top-bar></top-bar>
+
     <div class="search">
       <search></search>
     </div>
+
     <div class="card-list">
       <h3 class="section-title">
-        Best bots of the month
+        {{ best_of_month_title }}
       </h3>
+
+      <card-bot></card-bot>
     </div>
 
-    <div class="badge badge--danger">Danger</div>
-    <card-bot></card-bot>
+    <!--<div class="badge badge&#45;&#45;danger">Danger</div>-->
 
-  <section class="top-authors">
-    <h3 class="section-title">
-      Top Authors
-    </h3>
-    <div class="authors-list">
-      <div class="card-author">
-        <div class="card-author__image">
-          <img src="../assets/images/man.png" alt="">
-        </div>
-        <div class="card-author__info">
-          <div class="author__name">Matthew Russell</div>
-          <div class="author__info">
-          <span class="author__location">
-            Granada, Andalusia, Spain
-          </span>
-            <span class="author__joined">
-            Joined 2 years ago · last seen 9 days ago
-          </span>
-          </div>
-          <a href="#" class="author__url">http://davidgasquez.buthub.api/</a>
-          <span class="author__bot-counts">60 bots</span>
-        </div>
+    <section class="top-authors">
+      <h3 class="section-title">
+        {{ top_authors_title }}
+      </h3>
+      <div class="authors-list">
+        <card-author></card-author>
       </div>
-      <div class="card-author">
-        <div class="card-author__image">
-          <img src="../assets/images/man.png" alt="">
-        </div>
-        <div class="card-author__info">
-          <div class="author__name">Matthew Russell</div>
-          <div class="author__info">
-          <span class="author__location">
-            Granada, Andalusia, Spain
-          </span>
-            <span class="author__joined">
-            Joined 2 years ago · last seen 9 days ago
-          </span>
-          </div>
-          <a href="#" class="author__url">http://davidgasquez.buthub.api/</a>
-          <span class="author__bot-counts">60 bots</span>
-        </div>
-      </div>
-      <div class="card-author">
-        <div class="card-author__image">
-          <img src="../assets/images/man.png" alt="">
-        </div>
-        <div class="card-author__info">
-          <div class="author__name">Matthew Russell</div>
-          <div class="author__info">
-          <span class="author__location">
-            Granada, Andalusia, Spain
-          </span>
-            <span class="author__joined">
-            Joined 2 years ago · last seen 9 days ago
-          </span>
-          </div>
-          <a href="#" class="author__url">http://davidgasquez.buthub.api/</a>
-          <span class="author__bot-counts">60 bots</span>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
+
   </div>
 </template>
 
 <script>
 import CardBot from './CardBot.vue'
+import CardAuthor from './CardAuthor.vue'
 import Search from './Search.vue'
 import TopBar from './TopBar.vue'
 
@@ -87,13 +38,14 @@ export default {
   name: 'HomePage',
   components: {
     CardBot,
+    CardAuthor,
     Search,
     TopBar
   },
   data () {
     return {
-      mensagem: 'Robot Society',
-      subtitulo: 'TRAIN YOURS'
+      best_of_month_title: 'Best bots of the month',
+      top_authors_title: 'Top Authors'
     }
   }
 }
@@ -102,78 +54,4 @@ export default {
 <style scoped lang="scss">
   @import '../assets/scss/_variables';
   @import '../assets/scss/_colors';
-  h1 {
-    font-family: $font-secondary;
-    width: 100%;
-    position: relative;
-    margin: 0 auto;
-    font-weight: normal;
-    background-color: #42b983;
-    text-align: center;
-    span {
-      color: #FFF;
-      display: block;
-      font-size: 16px;
-      font-weight: 300;
-    }
-  }
-/*Card Author */
-  .authors-list {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .card-author {
-    background-color: #fff;
-    border-radius: 10px;
-    width: calc(33.33% - 30px);
-    padding: 20px;
-    display: flex;
-    margin: 15px;
-
-    @media (max-width: 992px) {
-      width: calc(50% - 30px);
-    }
-
-    @media (max-width: 768px) {
-      width: 100%;
-      margin: 10px 0;
-    }
-  }
-  .card-author__image {
-    width: 100px;
-    margin-right: 20px;
-    overflow: hidden;
-    border-radius: 10px;
-    img {
-      width: 100%;
-    }
-  }
-  .card-author__info {
-    width: calc(100% - 120px);
-    font-size: 10px;
-    line-height: 16px;
-  }
-  .author__name {
-    font-size: 14px;
-    font-weight: 500;
-    margin-bottom: 10px;
-  }
-  .author__location {
-    display: block;
-  }
-  .author__joined {
-    display: block;
-  }
-  .author__url {
-    display: block;
-    color: #000;
-    text-decoration: none;
-    &:hover {
-      color: $primary-color;
-    }
-  }
-  .author__bot-counts {
-    display: block;
-    margin-top: 10px;
-  }
 </style>
