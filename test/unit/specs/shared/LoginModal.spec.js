62d653a61ed58f56bf486896c4744cc0976fc2ee
Douglas Paz <eu@douglaspaz.com>
2018-04-02 13:51:12 -0300

    Refactor LoginModal test

--- test/unit/specs/shared/LoginModal.spec.js
+++ test/unit/specs/shared/LoginModal.spec.js
@@ -1,4 +1,3 @@
-import Vue from 'vue';
 import Vuex from 'vuex';
 import { shallow, createLocalVue } from '@vue/test-utils';
 
@@ -8,37 +7,70 @@ import LoginModal from '@/components/shared/LoginModal';
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-const getWrapper = () => {
-  store.replaceState({
-    LoginModal: {
-      open: false,
-      next: null,
-    },
-  });
-  return shallow(LoginModal, { store, localVue });
-};
-
 describe('LoginModal.vue', () => {
-  it('onAuthenticated', (done) => {
-    const wrapper = getWrapper();
+  describe('close modal on authenticated', () => {
+    describe('without next', () => {
+      let wrapper;
+      beforeEach(() => {
+        store.replaceState({
+          LoginModal: {
+            open: false,
+            next: null,
+          },
+        });
+        wrapper = shallow(LoginModal, { store, localVue });
+        store.dispatch('openLoginModal');
+        wrapper.vm.onAuthenticated();
+      });
 
-    Vue.nextTick(() => {
-      wrapper.vm.onAuthenticated();
-      expect(store.getters.loginModalOpen)
-        .toBeFalsy();
-      done();
+      test('closed', () => {
+        expect(store.getters.loginModalOpen)
+          .toBeFalsy();
+      });
+    });
+
+    describe('with next', () => {
+      let wrapper;
+      beforeEach(() => {
+        store.replaceState({
+          LoginModal: {
+            open: false,
+            next: null,
+          },
+        });
+        wrapper = shallow(LoginModal, { store, localVue });
+        store.dispatch('openLoginModal', { path: '/myprofile/' });
+        wrapper.vm.onAuthenticated();
+      });
+
+      test('closed', () => {
+        expect(store.getters.loginModalOpen)
+          .toBeFalsy();
+      });
     });
   });
 
-  it('onAuthenticated with next', (done) => {
-    const wrapper = getWrapper();
-    store.dispatch('openLoginModal', { path: '/myprofile/' });
+  describe('tabs', () => {
+    let wrapper;
+    beforeEach(() => {
+      store.replaceState({
+        LoginModal: {
+          open: false,
+          next: null,
+        },
+      });
+      wrapper = shallow(LoginModal, { store, localVue });
+    });
 
-    Vue.nextTick(() => {
-      wrapper.vm.onAuthenticated();
-      expect(store.getters.loginModalOpen)
-        .toBeFalsy();
-      done();
+    test('default: login visible', () => {
+      expect(wrapper.vm.tab === 'login')
+        .toBeTruthy();
+    });
+
+    test('forgot password visible', () => {
+      wrapper.vm.showForgotPassword();
+      expect(wrapper.vm.tab === 'forgot-password')
+        .toBeTruthy();
     });
   });
 });
