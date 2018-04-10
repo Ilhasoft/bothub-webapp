f6f51a957cf04729f78ff05679ab67d067af0290
Douglas Paz <eu@douglaspaz.com>
2018-04-10 15:40:53 -0300

    Use b-modal

--- src/components/shared/LoginModal.vue
+++ src/components/shared/LoginModal.vue
@@ -1,26 +1,28 @@
 <template>
-  <modal ref="loginModal">
-    <div v-show="tab === 'login'">
-      <login-form @authenticated="onAuthenticated" />
-      <p><a href="" @click.prevent="showForgotPassword">forgot password?!</a></p>
+  <b-modal :active="open" @close="closeLoginModal">
+    <div class="card">
+      <div class="card-content">
+        <div v-show="tab === 'login'">
+          <login-form @authenticated="onAuthenticated" />
+          <p><a href="" @click.prevent="showForgotPassword">forgot password?!</a></p>
+        </div>
+        <div v-show="tab === 'forgot-password'">
+          <forgot-password-form />
+        </div>
+        <p><button
+          ref="cancelBtn"
+          @click="closeLoginModal">cancel</button></p>
+      </div>
     </div>
-    <div v-show="tab === 'forgot-password'">
-      <forgot-password-form />
-    </div>
-    <p><button
-      ref="cancelBtn"
-      @click="closeLoginModal">cancel</button></p>
-  </modal>
+  </b-modal>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import Modal from '@/components/shared/Modal';
 import LoginForm from '@/components/auth/LoginForm';
 import ForgotPasswordForm from '@/components/auth/ForgotPasswordForm';
 
 const components = {
-  Modal,
   LoginForm,
   ForgotPasswordForm,
 };
@@ -36,7 +38,6 @@ export default {
   computed: {
     ...mapGetters({
       open: 'loginModalOpen',
-      next: 'loginModalNext',
     }),
   },
   methods: {
