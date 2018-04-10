cec3e95562959ba61991feb5865bc0dd532f0032
Douglas Paz <eu@douglaspaz.com>
2018-04-10 15:24:34 -0300

    Add Buefy as dependencie

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'buefy/lib/buefy.css';
+
 import Vue from 'vue';
+import Buefy from 'buefy';
+
 import App from './App';
 import router from './router';
 import store from './store';
 
 import AUTH_TYPES from './store/auth/types';
 
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -14,7 +17,7 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>',
+  template: '<App />',
   store,
   beforeCreate() {
     this.$store.commit(AUTH_TYPES.RETRIEVE_AUTH_TOKEN);
