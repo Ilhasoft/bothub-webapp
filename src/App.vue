8fe6e6226945bac537c85e02b9689355ee94b66f
Douglas Paz <eu@douglaspaz.com>
2018-04-10 15:31:02 -0300

    Remove App custom style

--- src/App.vue
+++ src/App.vue
@@ -25,17 +25,4 @@ export default {
 </script>
 
 <style lang="scss">
-html,
-body,
-#app {
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: sans-serif;
-  color: #2c3e50;
-}
 </style>
