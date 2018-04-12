651d05d804a4058fa2840341b20e39b22b99c6d8
Douglas Paz <eu@douglaspaz.com>
2018-04-12 10:00:04 -0300

    Update updateMyProfile to nullable nicknameAuthenticated state

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -3,12 +3,16 @@ import TYPES from '../types';
 
 export default {
   async updateMyProfile({ commit }) {
-    const response = await auth.myProfile();
-    commit(TYPES.SET_PROFILE, {
-      nickname: response.data.nickname,
-      data: response.data,
-    });
-    commit(TYPES.SET_NICKNAME_AUTHENTICATED, { nickname: response.data.nickname });
+    try {
+      const response = await auth.myProfile();
+      commit(TYPES.SET_PROFILE, {
+        nickname: response.data.nickname,
+        data: response.data,
+      });
+      commit(TYPES.SET_NICKNAME_AUTHENTICATED, { nickname: response.data.nickname });
+    } catch (error) {
+      commit(TYPES.SET_NICKNAME_AUTHENTICATED, { nickname: null });
+    }
   },
   async updateProfile({ commit }, { nickname }) {
     const response = await auth.profile(nickname);
