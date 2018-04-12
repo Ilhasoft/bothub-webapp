de0d3db81405d977ebfa69c31a6f301e94b839dc
Douglas Paz <eu@douglaspaz.com>
2018-04-12 13:36:31 -0300

    Fix updateMyProfile

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -2,16 +2,20 @@ import auth from '@/api/auth';
 import TYPES from '../types';
 
 export default {
-  async updateMyProfile({ commit }) {
-    try {
-      const response = await auth.myProfile();
-      commit(TYPES.SET_PROFILE, {
-        nickname: response.data.nickname,
-        data: response.data,
-      });
-      commit(TYPES.SET_NICKNAME_AUTHENTICATED, { nickname: response.data.nickname });
-    } catch (error) {
+  async updateMyProfile({ commit, dispatch, getters }) {
+    if (!getters.authenticated) {
       commit(TYPES.SET_NICKNAME_AUTHENTICATED, { nickname: null });
+    } else {
+      try {
+        const response = await auth.myProfile();
+        commit(TYPES.SET_PROFILE, {
+          nickname: response.data.nickname,
+          data: response.data,
+        });
+        commit(TYPES.SET_NICKNAME_AUTHENTICATED, { nickname: response.data.nickname });
+      } catch (error) {
+        dispatch('logout');
+      }
     }
   },
   async updateProfile({ commit }, { nickname }) {
