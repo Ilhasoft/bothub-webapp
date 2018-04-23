460d734e627cdfb02de97acaf3c22848acd054aa
Douglas Paz <eu@douglaspaz.com>
2018-04-23 10:47:21 -0300

    Add search bar in home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <layout></layout>
+  <layout>
+    <header>
+      <h1 class="title has-text-white has-text-centered">Robot Society</h1>
+      <h2 class="subtitle has-text-white has-text-weight-light has-text-centered">TRAIN YOURS!</h2>
+    </header>
+    <div class="container"><search-bar /></div>
+    <div class="wrapper"></div>
+  </layout>
 </template>
 
 <script>
 import Layout from '@/components/shared/Layout';
+import SearchBar from '@/components/shared/SearchBar';
 
 const components = {
   Layout,
+  SearchBar,
 };
 
 export default {
@@ -15,3 +24,15 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+header {
+  margin-bottom: 3rem;
+  padding: 0 8px;
+}
+
+.wrapper {
+  margin-top: -25px;
+  background-color: #f8f8f8;
+  min-height: 200px;
+}
+</style>
