d8a675a9134dcf4c7bec7adbdbc1a6d29234e07b
Douglas Paz <eu@douglaspaz.com>
2018-04-30 13:54:29 -0300

    Add Loading component

---
+++ src/components/shared/Loading.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="spinner">
+    <div class="bounce"></div>
+    <div class="bounce"></div>
+    <div class="bounce"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/utilities.scss';
+
+.spinner {
+  margin: 1.5rem auto;
+  width: 70px;
+  text-align: center;
+
+  .bounce {
+    width: 18px;
+    height: 18px;
+    background-color: $grey-light;
+
+    border-radius: 100%;
+    display: inline-block;
+    animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+
+    &:nth-child(1) {
+      animation-delay: -0.32s;
+    }
+
+    &:nth-child(2) {
+      animation-delay: -0.16s;
+    }
+  }
+}
+
+@keyframes sk-bouncedelay {
+  0%, 80%, 100% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  } 40% {
+    -webkit-transform: scale(1.0);
+    transform: scale(1.0);
+  }
+}
+</style>
+
