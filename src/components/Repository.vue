448a9461ea2f6dd257d87c5e96d9a841c4bcdf42
Douglas Paz <eu@douglaspaz.com>
2018-05-08 10:20:50 -0300

    Add navbar and tabs in Repository router

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -9,8 +9,40 @@
         :name="repository.name"
         :owner__nickname="repository.owner__nickname"
         :available_languages="repository.available_languages"
-        :description="repository.description"
         :categories_list="repository.categories_list" />
+      <ul class="navbar">
+        <li
+          @click="activeTab = 0"
+          :class="{'navbar-item': true, 'active': activeTab === 0}">Status</li>
+        <li
+          @click="activeTab = 1"
+          :class="{'navbar-item': true, 'active': activeTab === 1}">Examples</li>
+        <li
+          @click="activeTab = 2"
+          :class="{'navbar-item': true, 'active': activeTab === 2}">Translate</li>
+        <li
+          @click="activeTab = 3"
+          :class="{'navbar-item': true, 'active': activeTab === 3}">Update</li>
+        <li
+          @click="activeTab = 4"
+          :class="{'navbar-item': true, 'active': activeTab === 4}">Issues</li>
+        <li
+          @click="activeTab = 5"
+          :class="{'navbar-item': true, 'active': activeTab === 5}">Historic</li>
+      </ul>
+      <b-tabs
+        v-model="activeTab"
+        class="hide-tabs-nav">
+        <b-tab-item>
+          <p v-if="repository.description">{{ repository.description }}</p>
+          <p v-else>No description.</p>
+        </b-tab-item>
+        <b-tab-item>Examples</b-tab-item>
+        <b-tab-item>Translate</b-tab-item>
+        <b-tab-item>Update</b-tab-item>
+        <b-tab-item>Issues</b-tab-item>
+        <b-tab-item>Historic</b-tab-item>
+      </b-tabs>
     </div>
     <div
       v-else-if="!hasError"
@@ -49,6 +81,7 @@ export default {
   data() {
     return {
       repository: null,
+      activeTab: 0,
       hasError: false,
       errorDetail: null,
     };
@@ -95,4 +128,62 @@ export default {
     padding: 8px;
   }
 }
+
+.navbar {
+  $primary-darken: darken($primary, 5%);
+
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+
+  border-bottom: 1px solid $primary-darken;
+
+  @media (max-width: $desktop) {
+    overflow-x: scroll;
+    justify-content: flex-start;
+  }
+
+  &-item {
+    position: relative;
+    display: block;
+    min-width: 100px;
+    padding: 16px 16px 24px;
+    color: $grey;
+    font-size: .8rem;
+    text-align: center;
+    cursor: pointer;
+
+    &::before {
+      content: '';
+      position: absolute;
+      left: 50%;
+      width: 100%;
+      padding-top: 50%;
+      bottom: -40%;
+      transform: translate(-50%, 70%);
+      background-color: $primary-darken;
+      border-radius: 50%;
+      transition: bottom .2s ease;
+    }
+
+    &:hover,
+    &.active {
+      color: $primary-darken;
+
+      &::before {
+        bottom: 0;
+      }
+    }
+  }
+}
 </style>
+
+<style lang="scss">
+.hide-tabs-nav {
+  > nav.tabs {
+    display: none;
+  }
+}
+</style>
+
