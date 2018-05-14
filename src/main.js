30690bf9bc05835d21fd43f8a75487818803c15a
Douglas Paz <eu@douglaspaz.com>
2018-05-14 11:23:18 -0300

    Add vue moment

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import '@mdi/font/css/materialdesignicons.css';
 
 import Vue from 'vue';
 import Buefy from 'buefy';
+import VueMoment from 'vue-moment';
 
 import App from './App';
 import router from './router';
 import store from './store';
 
 Vue.use(Buefy);
+Vue.use(VueMoment);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
