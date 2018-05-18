ec978d19942965417dc058491622d070d5a527c0
Douglas Paz <eu@douglaspaz.com>
2018-05-18 14:03:20 -0300

    Add percent filter

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -4,4 +4,7 @@ import { LANGUAGES } from '.';
 export default (Vue) => {
   /* istanbul ignore next */
   Vue.filter('languageVerbose', lang => LANGUAGES[lang]);
+
+  /* istanbul ignore next */
+  Vue.filter('percent', value => `${(value * 100).toFixed(1)}%`);
 };
