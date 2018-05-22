f0b2c62fd5e3ee617b004cab71b98887ca7c4cef
Douglas Paz <eu@douglaspaz.com>
2018-05-22 14:18:41 -0300

    Fix set profile mutation

--- src/store/user/mutations.js
+++ src/store/user/mutations.js
@@ -2,7 +2,7 @@ import TYPES from '../types';
 
 export default {
   [TYPES.SET_PROFILE](state, { nickname, data }) {
-    state.profiles = Object.assign({ [nickname]: data }, state.profiles);
+    state.profiles = Object.assign({}, state.profiles, { [nickname]: data });
   },
   [TYPES.SET_NICKNAME_AUTHENTICATED](state, { nickname }) {
     state.nicknameAuthenticated = nickname;
