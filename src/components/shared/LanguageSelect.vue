af66978db4ad3b03ecd9f0f091ed8b86d564d3a1
Douglas Paz <eu@douglaspaz.com>
2018-05-25 13:37:24 -0300

    Add LanguageSelect cmoponent

---
+++ src/components/shared/LanguageSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-select
+    expanded
+    @input="$emit('input', $event)"
+    :value="value">
+    <option :value="null">-</option>
+    <option
+      v-for="(label, language) in languages"
+      :value="language"
+      :key="language">{{ label }}</option>
+  </b-select>
+</template>
+
+<script>
+import { LANGUAGES } from '@/utils';
+
+
+export default {
+  name: 'LanguageSelect',
+  props: {
+    value: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      languages: LANGUAGES,
+    };
+  },
+};
+</script>
+
---
+++ test/unit/specs/shared/LanguageSelect.spec.js
@@ -0,0 +1,19 @@
+import Buefy from 'buefy';
+
+import { shallow, createLocalVue } from '@vue/test-utils';
+import LanguageSelect from '@/components/shared/LanguageSelect';
+
+
+const localVue = createLocalVue();
+localVue.use(Buefy);
+
+describe('LanguageSelect.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(LanguageSelect, { localVue });
+  });
+
+  test('mount', () => {
+    expect(wrapper.vm).toBeDefined();
+  });
+});
