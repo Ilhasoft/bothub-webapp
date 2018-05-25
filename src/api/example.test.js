be7148b7dd08164a36efccd7c4ea58edbd42e46f
Douglas Paz <eu@douglaspaz.com>
2018-05-25 10:34:57 -0300

    Add api.example.search

--- src/api/example.test.js
+++ src/api/example.test.js
@@ -28,4 +28,9 @@ describe('api/example', () => {
     const response = await example.delete(1);
     expect(response.status).toBe(204);
   });
+  test('search', async () => {
+    const list = example.search('8511fd26-a3bc-4f74-9af1-176abca5401d');
+    await list.next();
+    expect(list.items).toHaveLength(1);
+  });
 });
