958a2385cd0ee6a1fbab0b25e0393d984f5a11bf
Douglas Paz <eu@douglaspaz.com>
2018-05-29 15:45:22 -0300

    Create TranslationsList component

---
+++ test/unit/specs/translate/TranslationsList.spec.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/first */
+jest.mock('@/api/request');
+
+import Vuex from 'vuex';
+import { shallow, createLocalVue } from '@vue/test-utils';
+import TranslationsList from '@/components/translate/TranslationsList';
+import applyFilters from '@/utils/filters';
+import store from '@/store';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+applyFilters(localVue);
+
+describe('TranslationsList.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(TranslationsList, {
+      localVue,
+      store,
+      propsData: {
+        repository: {
+          uuid: '8511fd26-a3bc-4f74-9af1-176abca5401d',
+        },
+      },
+    });
+  });
+
+  test('mount', () => {
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  describe('update translations', () => {
+    beforeEach(async () => {
+      await wrapper.vm.updateTranslations();
+    });
+
+    test('has translations list', () => {
+      expect(wrapper.vm.translationsList).not.toBe(null);
+    });
+  });
+});
