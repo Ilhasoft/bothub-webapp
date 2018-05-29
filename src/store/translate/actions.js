7e6e3f6814b50a777316d5992ed5b164f0142891
Douglas Paz <eu@douglaspaz.com>
2018-05-29 15:54:12 -0300

    Fix query in getTranslations action

--- src/store/translate/actions.js
+++ src/store/translate/actions.js
@@ -5,7 +5,7 @@ export default {
     const response = await translate.new(exampleId, language, text, entities);
     return response;
   },
-  async getTranslations(store, { repositoryUuid, query }) {
+  async getTranslations(store, { repositoryUuid, ...query }) {
     return translate.translations(repositoryUuid, query);
   },
   async deleteTranslation(store, { translationId }) {
