437191401966e2ddb4cb99e7f3c3e106fbccf077
Douglas Paz <eu@douglaspaz.com>
2018-06-01 13:56:07 -0300

    Update RepositoryAvatar, now can vote in repository

--- src/components/repository/RepositoryAvatar.vue
+++ src/components/repository/RepositoryAvatar.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="avatar">
     <div class="avatar-updownvote">
-      <button class="avatar-updownvote-btn">
+      <button
+        @click="onUp()"
+        class="avatar-updownvote-btn">
         <b-icon icon="chevron-up" />
       </button>
-      <div class="avatar-updownvote-value">0</div>
-      <button class="avatar-updownvote-btn">
+      <div class="avatar-updownvote-value">{{ finalVotesSum || 0 }}</div>
+      <button
+        @click="onDown()"
+        class="avatar-updownvote-btn">
         <b-icon icon="chevron-down" />
       </button>
     </div>
@@ -16,11 +20,80 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'RepositoryAvatar',
   props: {
     name: {
       type: String,
+      default: '...',
+    },
+    ownerNickname: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    votesSum: {
+      type: Number,
+      default: 0,
+    },
+  },
+  watch: {
+    votesSum() {
+      /* istanbul ignore next */
+      this.finalVotesSum = this.votesSum;
+    },
+  },
+  data() {
+    return {
+      finalVotesSum: this.votesSum,
+    };
+  },
+  methods: {
+    ...mapActions([
+      'repositoryVoteUp',
+      'repositoryVoteDown',
+    ]),
+    async voteHandler(fn) {
+      const { ownerNickname, slug } = this;
+      try {
+        const response = await fn({
+          ownerNickname,
+          slug,
+        });
+        const { data: { votes_sum: votesSum } } = response;
+        if (votesSum !== undefined) {
+          this.finalVotesSum = votesSum;
+        }
+        return true;
+      } catch (e) {
+        const { data } = e.response || {};
+        if (data && data.detail) {
+          this.$toast.open({
+            message: data.detail,
+            type: 'is-danger',
+          });
+        } else {
+          /* istanbul ignore next */
+          this.$toast.open({
+            message: 'The vote was not registered.',
+            type: 'is-danger',
+          });
+        }
+      }
+      return false;
+    },
+    async onUp() {
+      const r = await this.voteHandler(this.repositoryVoteUp);
+      return r;
+    },
+    async onDown() {
+      const r = await this.voteHandler(this.repositoryVoteDown);
+      return r;
     },
   },
 };
