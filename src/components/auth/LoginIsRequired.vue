797569124402828c591d169ade436e2a3888cb24
Douglas Paz <eu@douglaspaz.com>
2018-06-04 16:05:47 -0300

    Create LoginIsRequired component

---
+++ src/components/auth/LoginIsRequired.vue
@@ -0,0 +1,19 @@
+<script>
+import { mapGetters } from 'vuex';
+
+
+export default {
+  watch: {
+    authenticated() {
+      /* istanbul ignore next */
+      if (!this.authenticated) {
+        /* istanbul ignore next */
+        this.$store.dispatch('openLoginModal', {
+          redirectToWhenFails: { name: 'home' },
+        });
+      }
+    },
+  },
+  computed: mapGetters(['authenticated']),
+};
+</script>
