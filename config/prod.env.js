0393e53d0d49fad0f297e6c626e039ab0a2cea66
Douglas Paz <eu@douglaspaz.com>
2018-06-08 15:47:25 -0300

    Update env vars config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: process.env.API_BASE_URL,
+  API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+  MAILCHIMP_LOGIN: JSON.stringify(process.env.MAILCHIMP_LOGIN),
+  MAILCHIMP_DATACENTER: JSON.stringify(process.env.MAILCHIMP_DATACENTER),
+  MAILCHIMP_USER_ID: JSON.stringify(process.env.MAILCHIMP_USER_ID),
+  MAILCHIMP_LIST_ID: JSON.stringify(process.env.MAILCHIMP_LIST_ID),
 }
