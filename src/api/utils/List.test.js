490366ff660b78d0b083f49c03a73536e173aae7
Douglas Paz <eu@douglaspaz.com>
2018-07-09 13:38:16 -0300

    Fix #108

--- src/api/utils/List.test.js
+++ src/api/utils/List.test.js
@@ -5,10 +5,41 @@ jest.mock('../request');
 import List from './List';
 
 describe('API List', () => {
-  test('repositories test', async () => {
-    const list = new List('/repositories/');
-    await list.next();
-    expect(list.items).toHaveLength(20);
-    expect(list.hasNext).toBeTruthy();
+  let list;
+  const initial = '/repositories/';
+  beforeEach(() => {
+    list = new List(initial);
+  });
+
+  describe('next', () => {
+    beforeEach(async () => {
+      await list.next();
+    });
+
+    test('has items', () => {
+      expect(list.items).toHaveLength(20);
+    });
+
+    test('has next', () => {
+      expect(list.hasNext).toBeTruthy();
+    });
+
+    describe('reset', () => {
+      beforeEach(() => {
+        list.reset();
+      });
+
+      test('nextEntryPoint is initial', () => {
+        expect(list.nextEntryPoint).toBe(initial);
+      });
+
+      test('empty items', () => {
+        expect(list.items).toHaveLength(0);
+      });
+
+      test('empty deletions', () => {
+        expect(list.deletions).toHaveLength(0);
+      });
+    });
   });
 });
