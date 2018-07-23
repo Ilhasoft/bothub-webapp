125a2e504ee3100902a1d1f6100b8f7c8a41c56a
Douglas Paz <eu@douglaspaz.com>
2018-07-23 13:41:33 -0300

    Refactor RegisterForm test

--- test/unit/specs/auth/RegisterForm.spec.js
+++ test/unit/specs/auth/RegisterForm.spec.js
@@ -1,64 +1,68 @@
 /* eslint-disable import/first */
 jest.mock('@/api/request');
 
-import Vuex from 'vuex';
-import Buefy from 'buefy';
-import { shallow, createLocalVue } from '@vue/test-utils';
-
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import store from '@/store';
 import RegisterForm from '@/components/auth/RegisterForm';
 
+
 const localVue = createLocalVue();
-localVue.use(Vuex);
-localVue.use(Buefy);
 
 describe('RegisterForm.spec.js', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(RegisterForm, { store, localVue });
+    wrapper = shallowMount(RegisterForm, { store, localVue });
   });
 
-  describe('register', () => {
-    describe('valid', () => {
-      beforeEach(() => {
-        wrapper.vm.data.email = 'new@user.com';
-        wrapper.vm.data.name = 'New';
-        wrapper.vm.data.nickname = 'new';
-        wrapper.vm.data.password = 'n123456';
-      });
+  test('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-      test('return true', async (done) => {
-        const r = await wrapper.vm.onSubmit();
-        expect(r).toBeTruthy();
-        done();
-      });
-
-      test('emit registered event', async (done) => {
-        await wrapper.vm.onSubmit();
-        expect(wrapper.emitted('registered').length)
-          .toBe(1);
-        done();
-      });
+  describe('fill form with valid data', () => {
+    beforeEach(() => {
+      wrapper.vm.data.email = 'new@user.com';
+      wrapper.vm.data.name = 'New';
+      wrapper.vm.data.nickname = 'new';
+      wrapper.vm.data.password = 'n123456';
     });
 
-    describe('invalid: user with email exists', () => {
-      beforeEach(() => {
-        wrapper.vm.data.email = 'fake@user.com';
-        wrapper.vm.data.name = 'Fake';
-        wrapper.vm.data.nickname = 'fake';
-        wrapper.vm.data.password = '123456';
+    describe('submit form', () => {
+      let r;
+      beforeEach(async () => {
+        r = await wrapper.vm.onSubmit();
       });
 
-      test('return false', async (done) => {
-        const r = await wrapper.vm.onSubmit();
+      test('return true', () => {
+        expect(r).toBeTruthy();
+      });
+
+      test('emit registered event', () => {
+        expect(wrapper.emitted('registered')).toBeDefined();
+      });
+    });
+  });
+
+  describe('fill form with invalid data', () => {
+    beforeEach(() => {
+      wrapper.vm.data.email = 'fake@user.com';
+      wrapper.vm.data.name = 'Fake';
+      wrapper.vm.data.nickname = 'fake';
+      wrapper.vm.data.password = '123456';
+    });
+
+
+    describe('submit form', () => {
+      let r;
+      beforeEach(async () => {
+        r = await wrapper.vm.onSubmit();
+      });
+
+      test('return false', () => {
         expect(r).toBeFalsy();
-        done();
       });
 
-      test('print error', async (done) => {
-        await wrapper.vm.onSubmit();
-        expect(wrapper.vm.errors.email.length).toBe(1);
-        done();
+      test('has error', () => {
+        expect(wrapper.vm.errors.email).not.toHaveLength(0);
       });
     });
   });
