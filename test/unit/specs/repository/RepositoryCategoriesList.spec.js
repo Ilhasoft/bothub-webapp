e4ae4f14f6aeafe08d4c95f7138553f48af98149
Douglas Paz <eu@douglaspaz.com>
2018-07-23 18:07:19 -0300

    Refactor RepositoryCategoriesList test

--- test/unit/specs/repository/RepositoryCategoriesList.spec.js
+++ test/unit/specs/repository/RepositoryCategoriesList.spec.js
@@ -1,15 +1,15 @@
 import Buefy from 'buefy';
-import { shallow, createLocalVue } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import RepositoryCategoriesList from '@/components/repository/RepositoryCategoriesList';
+
 
 const localVue = createLocalVue();
 localVue.use(Buefy);
 
-
 describe('RepositoryCategoriesList.vue', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(RepositoryCategoriesList, {
+    wrapper = shallowMount(RepositoryCategoriesList, {
       localVue,
       propsData: {
         categories: [
