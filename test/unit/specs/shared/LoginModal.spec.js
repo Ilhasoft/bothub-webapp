949e52737fc53e4cd343bbd57395d4cf3c455de2
Douglas Paz <eu@douglaspaz.com>
2018-07-24 09:16:13 -0300

    Refactor LoginModal test

--- test/unit/specs/shared/LoginModal.spec.js
+++ test/unit/specs/shared/LoginModal.spec.js
@@ -1,16 +1,12 @@
-import Vuex from 'vuex';
 import Buefy from 'buefy';
-import Router from 'vue-router';
-import { shallow, createLocalVue } from '@vue/test-utils';
-
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import store from '@/store';
 import router from '@/router';
 import LoginModal from '@/components/shared/LoginModal';
 
+
 const localVue = createLocalVue();
-localVue.use(Vuex);
 localVue.use(Buefy);
-localVue.use(Router);
 
 describe('LoginModal.vue', () => {
   let wrapper;
@@ -22,11 +18,15 @@ describe('LoginModal.vue', () => {
         redirectToWhenFails: null,
       },
     });
-    wrapper = shallow(LoginModal, {
+    wrapper = shallowMount(LoginModal, {
       store,
       router,
       localVue,
     });
+  });
+
+  test('mount', () => {
+    expect(wrapper.vm).toBeDefined();
   });
 
   describe('simple', () => {
