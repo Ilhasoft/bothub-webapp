ad73d87c6b06a782011c99738805137c23cfd81e
Douglas Paz <eu@douglaspaz.com>
2018-08-02 10:58:05 -0300

    Add direction in BhTooltip

--- src/bh/components/BhTooltip.vue
+++ src/bh/components/BhTooltip.vue
@@ -4,7 +4,7 @@
     :tabindex="_uid"
     @click="close()"
     @blur="close()"
-    class="bh-tooltip">
+    :class="classAttr">
     <slot />
   </div>
 </template>
@@ -16,6 +16,10 @@ export default {
     open: {
       type: Boolean,
       default: false,
+    },
+    direction: {
+      type: String,
+      default: null,
     },
   },
   watch: {
@@ -32,6 +36,17 @@ export default {
       this.$emit('update:open', false);
     },
   },
+  computed: {
+    classAttr() {
+      const classes = ['bh-tooltip'];
+
+      if (this.direction) {
+        classes.push(`bh-tooltip--${this.direction}`);
+      }
+
+      return classes;
+    },
+  },
 };
 </script>
 
@@ -40,14 +55,31 @@ export default {
 
 .bh {
   &-tooltip {
+    $background-color: black;
+
     z-index: $tooltip-z-index;
     display: block;
     padding: 8px 12px;
     color: white;
-    background-color: black;
+    background-color: $background-color;
     border-radius: 4px;
     outline: none;
     box-shadow: 0 0 16px -2px rgba(0, 0, 0, 1);
+
+    &--down {
+      &::before {
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        width: 0;
+        height: 0;
+        content: '';
+        border-top: 6px solid $background-color;
+        border-right: 6px solid transparent;
+        border-left: 6px solid transparent;
+        transform: translate(-50%, 100%);
+      }
+    }
   }
 }
 </style>
