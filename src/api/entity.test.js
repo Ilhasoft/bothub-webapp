27ca975c974d61f73255668e82fa49483ab00bcc
Douglas Paz <eu@douglaspaz.com>
2018-08-08 09:34:58 -0300

    Fix api/entities tests

--- src/api/entity.test.js
+++ src/api/entity.test.js
@@ -4,7 +4,7 @@ jest.mock('./request');
 
 import entity from './entity';
 
-describe('api/example', () => {
+describe('api/entity', () => {
   test('search', async () => {
     const list = entity.search('8511fd26-a3bc-4f74-9af1-176abca5401d');
     await list.next();
