1cf67e8c1370bb1adaa41a0f4e157a14cb418c7b
Douglas Paz <eu@douglaspaz.com>
2018-08-15 10:02:53 -0300

    Add ExampleTextWithHighlightedEntitiesInput test

---
+++ test/unit/specs/components/inputs/ExampleTextWithHighlightedEntitiesInput.spec.js
@@ -0,0 +1,67 @@
+import BH from 'bh';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import ExampleTextWithHighlightedEntitiesInput from '@/components/inputs/ExampleTextWithHighlightedEntitiesInput';
+
+
+const localVue = createLocalVue();
+localVue.use(BH);
+
+describe('ExampleTextWithHighlightedEntitiesInput.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(ExampleTextWithHighlightedEntitiesInput, { localVue });
+  });
+
+  test('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('set text', () => {
+    const textareaValue = 'hi kids';
+    beforeEach(() => {
+      const textarea = wrapper.find({ ref: 'textarea' });
+      textarea.element.value = textareaValue;
+      textarea.trigger('input');
+    });
+
+    test(`val is ${textareaValue}`, () => {
+      expect(wrapper.vm.val).toBe(textareaValue);
+    });
+
+    describe('select text', () => {
+      beforeEach(() => {
+        const textarea = wrapper.find({ ref: 'textarea' });
+        textarea.element.setSelectionRange(0, 2);
+      });
+
+      test('renders correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+
+      test('textSelected event emitted', () => {
+        expect(wrapper.emitted('textSelected')).toBeDefined();
+      });
+
+      describe('clear selected', () => {
+        beforeEach(() => {
+          wrapper.vm.clearSelected();
+        });
+
+        test('textSelected event value is null', () => {
+          const values = wrapper.emitted('textSelected');
+          expect(values.pop()[0]).toBeNull();
+        });
+      });
+    });
+
+    describe('with entity', () => {
+      beforeEach(() => {
+        wrapper.setProps({ entities: [{ start: 3, end: 7, entity: 'kids' }] });
+      });
+
+      test('renders correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+});
