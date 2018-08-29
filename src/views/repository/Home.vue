050ffad9843c689d3dff41411b036fe3b09ca1ed
Hugo <victordantas@Mac-mini-de-victor.local>
2018-08-29 14:22:00 -0300

    refactoring css class and integration with api

--- src/views/repository/Home.vue
+++ src/views/repository/Home.vue
@@ -3,54 +3,60 @@
     :repository="repository"
     :loading="loading"
     :error-code="errorCode">
-    <div class="main-wrapper">
-      <div class="main-wrapper__access-denied">
-        <div class="main-wrapper__access-denied">
-          <img
-            src="@/assets/imgs/mascot.svg"
-            alt=""
-            class="main-wrapper__access-denied__mascot-size">
-          <div class="main-wrapper__access-denied__message">
-            <p>You can’t contribute</p>
-            <p>and you can’t write.</p>
-          </div>
+    <div
+      v-if="repository"
+      class="repository-home">
+      <div class="repository-home__permissions">
+        <img
+          src="@/assets/imgs/mascot.svg"
+          alt="mascot"
+          class="repository-home__permissions__mascot">
+        <div class="repository-home__permissions__message">
+          <p>You can’t contribute</p>
+          <p>and you can’t write.</p>
         </div>
       </div>
-      <div class="main-wrapper__info">
-        <div class="main-wrapper__info__header">
-          <span><bh-icon value="flag-variant" /> 3 languuage</span>
-          <span><bh-icon value="botinho" /> 6 examples, created a day ago</span>
+      <div class="repository-home__info">
+        <div class="repository-home__info__header">
+          <span>
+            <bh-icon value="flag-variant" />
+            {{ repository.available_languages.length }} language
+          </span>
+          <span>
+            <bh-icon value="botinho" />
+            {{ repository.examples__count }} Examples, created a
+            {{ repository.created_at | moment('from') }}
+          </span>
         </div>
-        <div class="main-wrapper__info__description">
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error
-            sit voluptatem accusantium dolorelaudantium,
-            totam rem aperiam, eaque ipsa quae ab illo
-            inventore veritatis et quasi architecto beatae
-            vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-            quia voluptas sit aspernatur
-            aut odit aut fugit
-          </p>
+        <div class="repository-home__info__description">
+          <p v-if="repository.description">{{ repository.description }}</p>
+          <p v-else>There is no description for this repository</p>
         </div>
-        <h1 class="title is-3">Can identify</h1>
-        <div class="main-wrapper__info__i-and-c-wrapper bh-grid">
-          <div class="main-wrapper__info__i-and-c-wrapper bh-grid__item bh-grid__item--grow-5">
-            <h1 class="title is-4">Intenties</h1>
-            <div class="main-wrapper__info__i-and-c-wrapper__intenties">
+        <h1 class="bh-title-1">Can identify</h1>
+        <div class="bh-grid">
+          <div class="repository-home__info__attribute bh-grid__item">
+            <h1 class="bh-title-2">Intenties</h1>
+            <div class="repository-home__info__attribute__card">
               <bh-badge
+                v-for="(intents, index) in repository.intents"
+                :key="index"
                 size="small"
-                color="grey">
-                <span>Comprar</span>
+                color="grey"
+                class="repository-home__info__attribute__card__badge">
+                <span v-if="intents">{{ intents }}</span>
               </bh-badge>
             </div>
           </div>
-          <div class="main-wrapper__info__i-and-c-wrapper bh-grid__item bh-grid__item--grow-5">
-            <h1 class="title is-4">Categories</h1>
-            <div class="main-wrapper__info__i-and-c-wrapper__categories">
+          <div class="repository-home__info__attribute  bh-grid__item">
+            <h1 class="bh-title-2">Categories</h1>
+            <div class="repository-home__info__attribute__card">
               <bh-badge
+                v-for="(category, index) in repository.categories_list"
+                :key="index"
                 size="small"
-                color="grey">
-                <span>Vender</span>
+                color="grey"
+                class="repository-home__info__attribute__card__badge">
+                <span>{{ category }}</span>
               </bh-badge>
             </div>
           </div>
@@ -77,37 +83,36 @@ export default {
 </script>
 
 <style lang="scss">
-@import '~bh/assets/scss/grid.scss';
 @import '~bh/assets/scss/colors.scss';
 @import '~bh/assets/scss/variables.scss';
 
 
-.main-wrapper {
+.repository-home {
   box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.16);
 
-  &__access-denied {
+  &__permissions {
     background-color: $color-lighter-grey;
-    padding: 2rem;
+    padding: 3rem;
     display: flex;
     justify-content: center;
     align-items: center;
 
-     &__mascot-size {
-        width: 70px;
-        height: 70px;
-      }
+    &__mascot {
+      width: 70px;
+      height: 70px;
+    }
 
-      &__message {
-        margin-left: 1rem;
-      }
+    &__message {
+      margin-left: 1rem;
+    }
   }
 
   &__info {
     padding: 3rem;
-    color: $color-grey;
 
     &__header {
       margin-bottom: 4rem;
+      color: $color-grey;
 
       > span {
         margin-right: 4rem;
@@ -116,23 +121,24 @@ export default {
 
     &__description {
       margin-bottom: 4rem;
+      color: $color-grey;
     }
 
-    &__i-and-c-wrapper {
-      justify-content: space-between;
-      margin-top: 1rem;
-      color: $color-lighter-grey;
+    &__attribute {
+      flex-basis: 50%;
 
-      &__intenties {
+      &__card {
         background-color: $color-lighter-grey;
         border-radius: 6px;
-        padding: 1rem;
-      }
+        padding: .75rem;
+        margin: -.25rem;
 
-      &__categories {
-        background-color: $color-lighter-grey;
-        border-radius: 6px;
-        padding: 1rem;
+        &__badge {
+          margin: .25rem;
+          > span {
+            color: $color-primary-inverted
+          }
+        }
       }
     }
   }
