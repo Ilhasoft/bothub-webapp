0a051178e0511125c799c47cda0e76bd9c55b396
Douglas Paz <eu@douglaspaz.com>
2018-08-30 14:39:51 -0300

    Add home icon in bh

--- src/bh/utils.js
+++ src/bh/utils.js
@@ -9,6 +9,7 @@ import Equal from 'bh/assets/icons/equal.svg';
 import Account from 'bh/assets/icons/account.svg';
 import Botinho from 'bh/assets/icons/botinho.svg';
 import DotsVertical from 'bh/assets/icons/dots-vertical.svg';
+import Home from 'bh/assets/icons/home.svg';
 import FlagBr from 'bh/assets/icons/flag-br.svg';
 import FlagDe from 'bh/assets/icons/flag-de.svg';
 import FlagEs from 'bh/assets/icons/flag-es.svg';
@@ -31,6 +32,7 @@ export const icons = {
   account: Account,
   botinho: Botinho,
   'dots-vertical': DotsVertical,
+  home: Home,
   'flag-br': FlagBr,
   'flag-de': FlagDe,
   'flag-es': FlagEs,
