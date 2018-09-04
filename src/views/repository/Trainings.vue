b5af600249b92a07249bc74c268f9b6ea151e401
Douglas Paz <eu@douglaspaz.com>
2018-09-04 14:16:30 -0300

    Init training navigation

--- src/views/repository/Trainings.vue
+++ src/views/repository/Trainings.vue
@@ -6,9 +6,47 @@
     <div
       v-if="repository"
       class="trainings">
-      <intents-and-labels-list
-        :intents="repository.intents"
-        :labels="repository.labels" />
+      <div class="trainings__new-example">
+        <new-example-form :repository="repository" />
+      </div>
+      <div class="trainings__navigation">
+        <bh-navigation :actived.sync="currentPath">
+          <div label="Home">
+            <intents-and-labels-list
+              :intents="repository.intents"
+              :labels="repository.labels"
+              @showSentences="onShowSentences($event)" />
+          </div>
+          <div
+            slot="intent"
+            :label="currentIntent && `Intent: ${currentIntent.value}`">
+            <div v-if="currentIntent">
+              <h1>Intent {{ currentIntent.value }}</h1>
+              <pre>{{ currentIntent }}</pre>
+            </div>
+          </div>
+          <bh-navigation
+            slot="label"
+            :label="currentLabel && `Label: ${currentLabel.value}`">
+            <div v-if="currentLabel">
+              <h1>Label {{ currentLabel.value }}</h1>
+              <div>
+                <div
+                  v-for="entity in currentLabel.entities"
+                  :key="entity"><a @click.prevent="openEntity(entity)">{{ entity }}</a></div>
+              </div>
+              <pre>{{ currentLabel }}</pre>
+            </div>
+            <div
+              slot="entity"
+              :label="currentEntity && `Entity: ${currentEntity}`">
+              <div v-if="currentEntity">
+                {{ currentEntity }}
+              </div>
+            </div>
+          </bh-navigation>
+        </bh-navigation>
+      </div>
     </div>
   </repository-view-base>
 </template>
@@ -17,6 +55,7 @@
 import RepositoryBase from './Base';
 import RepositoryViewBase from '@/components/repository/RepositoryViewBase';
 import IntentsAndLabelsList from '@/components/repository/IntentsAndLabelsList';
+import NewExampleForm from '@/components/example/NewExampleForm';
 
 
 export default {
@@ -24,13 +63,54 @@ export default {
   components: {
     RepositoryViewBase,
     IntentsAndLabelsList,
+    NewExampleForm,
   },
   extends: RepositoryBase,
+  data() {
+    return {
+      currentPath: null,
+      currentIntent: null,
+      currentLabel: null,
+      currentEntity: null,
+    };
+  },
+  methods: {
+    onShowSentences({
+      type, intent, label, entity,
+    }) {
+      this.currentIntent = type === 'intent'
+        ? intent
+        : null;
+      this.currentLabel = ['label', 'entity'].includes(type)
+        ? label
+        : null;
+      this.currentEntity = type === 'entity'
+        ? entity
+        : null;
+      this.currentPath = type === 'entity'
+        ? 'label.entity'
+        : type;
+    },
+    openEntity(entity) {
+      this.currentEntity = entity;
+      this.currentPath = 'label.entity';
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import '~bh/assets/scss/colors.scss';
+
+
 .trainings {
-  padding: 1rem;
+  &__new-example {
+    padding: 1rem;
+    background-color: $color-lighter-grey;
+  }
+
+  &__navigation {
+    padding: 1rem;
+  }
 }
 </style>
