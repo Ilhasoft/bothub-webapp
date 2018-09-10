8026f7a697631ef6307d00d8063b1424d378654e
Hugo <victordantas@Mac-mini-de-victor.local>
2018-09-10 12:33:17 -0300

    add value to this.val

--- src/bh/components/BhText.vue
+++ src/bh/components/BhText.vue
@@ -67,7 +67,7 @@ export default {
   },
   mounted() {
     const { input } = this.$refs;
-    this.formatters.reduce((c, f) => f(c), this.value || '');
+    this.val = this.formatters.reduce((c, f) => f(c), this.value || '');
     if (input) {
       input.oninput = async () => {
         const formattedValue = this.formatters.reduce((c, f) => f(c), input.value);
