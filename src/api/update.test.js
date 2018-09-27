44c2cad33a959f000493189e7d3909ac7edd96af
Douglas Paz <eu@douglaspaz.com>
2018-09-27 09:57:47 -0300

    bl

--- src/api/update.test.js
+++ src/api/update.test.js
@@ -4,6 +4,7 @@ jest.mock('./request');
 
 import update from './update';
 
+
 describe('api/update.js', () => {
   test('search', async () => {
     const list = update.search('8511fd26-a3bc-4f74-9af1-176abca5401d');
