43f3872ee160eea56fcdd27338cde6cc52769608
Douglas Paz <eu@douglaspaz.com>
2018-10-01 14:54:23 -0300

    BH: update grid nested item documentation

--- bh/documentation/src/grid/examples/NestedItem.vue
+++ bh/documentation/src/grid/examples/NestedItem.vue
@@ -7,13 +7,15 @@
       <div class="bh-grid__item bh-grid__item--nested green item">Nested B</div>
       <div class="bh-grid__item red item">A</div>
       <div class="bh-grid__item red item">B</div>
-      <div class="bh-grid__item red item">C</div>
       <div class="bh-grid__item bh-grid__item--nested green item">Nested C</div>
+      <div class="bh-grid__item red item">C</div>
+      <div class="bh-grid__item red item">D</div>
+      <div class="bh-grid__item bh-grid__item--nested green item">Nested D</div>
     </div>
     <div class="bh-grid">
-      <div class="bh-grid__item bh-grid__item--nested pink item">Nested D</div>
-      <div class="bh-grid__item red item">D</div>
       <div class="bh-grid__item bh-grid__item--nested pink item">Nested E</div>
+      <div class="bh-grid__item red item">E</div>
+      <div class="bh-grid__item bh-grid__item--nested pink item">Nested F</div>
     </div>
   </div>
 </template>
