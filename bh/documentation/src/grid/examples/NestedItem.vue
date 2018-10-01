eaa96f39ec864e4fc0267389463ddbc9dd83b299
Douglas Paz <eu@douglaspaz.com>
2018-10-01 15:04:25 -0300

    BH: update grid system

--- bh/documentation/src/grid/examples/NestedItem.vue
+++ bh/documentation/src/grid/examples/NestedItem.vue
@@ -17,6 +17,10 @@
       <div class="bh-grid__item red item">E</div>
       <div class="bh-grid__item bh-grid__item--nested pink item">Nested F</div>
     </div>
+    <div class="bh-grid">
+      <div class="bh-grid__item bh-grid__item--nested red item">Nested G</div>
+      <div class="bh-grid__item bh-grid__item--nested purple item">Nested H</div>
+    </div>
   </div>
 </template>
 
