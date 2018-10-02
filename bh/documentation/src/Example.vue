5a90747d36f688d5f3c5891aba19b76f0c0a3287
Douglas Paz <eu@douglaspaz.com>
2018-10-02 10:00:39 -0300

    BH: init example component in docs

---
+++ bh/documentation/src/Example.vue
@@ -0,0 +1,30 @@
+<template>
+  <bh-card>
+    <div class="bh-grid">
+      <div class="bh-grid__item">
+        <div :is="component" />
+        <pre v-highlightjs="code"><code :class="codeClass" /></pre>
+      </div>
+    </div>
+  </bh-card>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  props: {
+    component: {
+      type: Object,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    codeClass: {
+      type: String,
+      default: 'html',
+    },
+  },
+};
+</script>
