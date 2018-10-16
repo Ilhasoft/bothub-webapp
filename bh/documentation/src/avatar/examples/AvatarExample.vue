1a342c5355d8b357bbb3bd9f54b77a17a91b656e
Victor Dantas <victor.dantas@ilhasoft.com.br>
2018-10-16 08:43:44 -0300

    BH:add avatar example

--- bh/documentation/src/avatar/examples/AvatarExample.vue
+++ bh/documentation/src/avatar/examples/AvatarExample.vue
@@ -6,6 +6,12 @@
         size="medium"
         value="botinho"/>
     </bh-avatar>
+    <bh-avatar
+      size="large">
+      <bh-icon
+        size="medium"
+        value="account"/>
+    </bh-avatar>
   </div>
 </template>
 
--- bh/documentation/src/avatar/examples/AvatarSize.vue
+++ bh/documentation/src/avatar/examples/AvatarSize.vue
@@ -1,33 +1,23 @@
 <template>
   <div>
-    <div class="bh-grid">
-      <div class="bh-grid__item item">
-        <bh-avatar size="small">
-          <bh-icon value="botinho"/>
-        </bh-avatar>
-      </div>
-      <div class="bh-grid__item item">
-        <bh-avatar size="normal">
-          <bh-icon
-            size="small"
-            value="botinho"/>
-        </bh-avatar>
-      </div>
-      <div class="bh-grid__item item">
-        <bh-avatar size="medium">
-          <bh-icon
-            size="normal"
-            value="botinho"/>
-        </bh-avatar>
-      </div>
-      <div class="bh-grid__item item">
-        <bh-avatar size="large">
-          <bh-icon
-            size="medium"
-            value="botinho"/>
-        </bh-avatar>
-      </div>
-    </div>
+    <bh-avatar size="small">
+      <bh-icon value="botinho"/>
+    </bh-avatar>
+    <bh-avatar size="normal">
+      <bh-icon
+        size="small"
+        value="botinho"/>
+    </bh-avatar>
+    <bh-avatar size="medium">
+      <bh-icon
+        size="normal"
+        value="botinho"/>
+    </bh-avatar>
+    <bh-avatar size="large">
+      <bh-icon
+        size="medium"
+        value="botinho"/>
+    </bh-avatar>
   </div>
 </template>
 
