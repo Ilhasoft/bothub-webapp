d5f9aa7f5e426f371b8841e88bcf48baeed9a6ac
Victor Dantas <victor.dantas@ilhasoft.com.br>
2018-10-16 09:57:00 -0300

    BH: Add efect to example button click

--- bh/documentation/src/button/examples/ButtonActionExample.vue
+++ bh/documentation/src/button/examples/ButtonActionExample.vue
@@ -1,20 +1,42 @@
 <template>
-  <div class="">
+  <div class="bh-grid">
     <bh-button
       secondary
       @click="buttonClicked()">
       Click here
     </bh-button>
+    <transition name="fade">
+      <p
+        v-if="show"
+        class="message">Button Clicked!</p>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SimpleButton',
+  data() {
+    return {
+      show: false,
+    };
+  },
   methods: {
     buttonClicked() {
-      alert('Button Clicked');
+      this.show = !this.show;
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.message{
+  margin: .5rem;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+</style>
