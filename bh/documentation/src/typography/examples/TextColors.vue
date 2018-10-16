85586b4417bd207586a733d12bb964be4621335a
Douglas Paz <eu@douglaspaz.com>
2018-10-16 09:15:10 -0300

    BH: update typography (docs)

---
+++ bh/documentation/src/typography/examples/TextColors.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p class="text-color-grey">.text-color-grey</p>
+    <p class="text-color-lighter-grey">.text-color-lighter-grey</p>
+    <p class="text-color-bolder-grey">.text-color-bolder-grey</p>
+    <p class="text-color-primary">.text-color-primary</p>
+    <p class="text-color-primary-inverted">.text-color-primary-inverted</p>
+    <p class="text-color-secondary">.text-color-secondary</p>
+    <p class="text-color-secondary-inverted">.text-color-secondary-inverted</p>
+    <p class="text-color-danger">.text-color-danger</p>
+    <p class="text-color-info">.text-color-info</p>
+    <p class="text-color-warning">.text-color-warning</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextColorsTypography',
+};
+</script>
