75ec850a4a7f0fc5fc2fc91958c23fe61b6bf0c9
Victor Dantas <victor.dantas@ilhasoft.com.br>
2018-10-23 10:33:13 -0300

    move trainResponse

--- src/components-v1/repository/TrainResponse.vue
+++ src/components-v1/repository/TrainResponse.vue
@@ -2,58 +2,69 @@
   <b-modal :active.sync="openValue">
     <bh-card v-if="!!trainResponse">
       <div class="bh-grid bh-grid--column">
-        <h1>Repository Trained!</h1>
-        <table>
-          <thead>
-            <tr>
-              <th colspan="2">Languages Report</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(data, language) in trainResponse.languages_report"
-              :key="language">
-              <td>{{ language | languageVerbose }}</td>
-              <td class="text-right">
-                <bh-icon
-                  v-if="data.status === 'trained'"
-                  value="check" />
-                <bh-icon
-                  v-else-if="data.status === 'not_ready_for_train'"
-                  value="dots-horizontal" />
-                <div v-else-if="data.status === 'failed'">
-                  <div>
-                    <bh-icon
-                      value="close" />
+        <div class="bh-grid__item">
+          <h1>Repository Trained!</h1>
+        </div>
+        <div class="bh-grid__item">
+          <table>
+            <thead>
+              <tr>
+                <th colspan="2">Languages Report</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(data, language) in trainResponse.languages_report"
+                :key="language">
+                <td>{{ language | languageVerbose }}</td>
+                <td class="text-right">
+                  <bh-icon
+                    v-if="data.status === 'trained'"
+                    class="text-color-primary"
+                    value="check" />
+                  <bh-icon
+                    v-else-if="data.status === 'not_ready_for_train'"
+                    class="text-color-info"
+                    value="dots-horizontal" />
+                  <div v-else-if="data.status === 'failed'">
+                    <div>
+                      <bh-icon
+                        value="close" />
+                    </div>
+                    <small>{{ data.error }}</small>
                   </div>
-                  <small>{{ data.error }}</small>
-                </div>
-                <span v-else>{{ data.status }}</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="bh-grid">
-          <div class="bh-grid__item">
-            <bh-icon
-              icon="check"
-              type="is-success"
-              size="is-small" />
-            <span>Trained</span>
-          </div>
-          <div class="bh-grid__item">
-            <bh-icon
-              icon="dots-horizontal"
-              type="is-info"
-              size="is-small" />
-            <span>No changes</span>
-          </div>
-          <div class="bh-grid__item">
-            <bh-icon
-              icon="close"
-              type="is-danger"
-              size="is-small" />
-            <span>Failed</span>
+                  <span v-else>{{ data.status }}</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="bh-grid__item bh-grid__item--nested">
+          <div class="bh-grid">
+            <div class="bh-grid__item">
+              <bh-icon
+                class="text-color-primary"
+                value="check"
+                type="is-success"
+                size="is-small" />
+              <span>Trained</span>
+            </div>
+            <div class="bh-grid__item">
+              <bh-icon
+                class="text-color-info"
+                value="dots-horizontal"
+                type="is-info"
+                size="is-small" />
+              <span>No changes</span>
+            </div>
+            <div class="bh-grid__item">
+              <bh-icon
+                class="text-color-danger"
+                value="close"
+                type="is-danger"
+                size="is-small" />
+              <span>Failed</span>
+            </div>
           </div>
         </div>
       </div>
