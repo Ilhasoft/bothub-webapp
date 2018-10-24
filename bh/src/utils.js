5b42b3530569928c734323b47587ea25ae36f639
Victor Dantas <victor.dantas@ilhasoft.com.br>
2018-10-24 09:45:49 -0300

    BH: add lock-reset icon

--- bh/src/utils.js
+++ bh/src/utils.js
@@ -23,6 +23,7 @@ import CodeTags from './assets/icons/code-tags.svg';
 import ClipboardTextOutline from './assets/icons/clipboard-text-outline.svg';
 import InvertColors from './assets/icons/invert-colors.svg';
 import ClipboardAlert from './assets/icons/clipboard-alert.svg';
+import LockReset from './assets/icons/lock-reset.svg';
 import Alert from './assets/icons/alert.svg';
 import FlagBr from './assets/icons/flag-br.svg';
 import FlagDe from './assets/icons/flag-de.svg';
@@ -63,6 +64,7 @@ export const icons = {
   'invert-colors': InvertColors,
   'clipboard-alert': ClipboardAlert,
   alert: Alert,
+  'lock-reset': LockReset,
   'flag-br': FlagBr,
   'flag-de': FlagDe,
   'flag-es': FlagEs,
