79ca5f76d24e311e73825df7bb5142579908cfc3
Douglas Paz <eu@douglaspaz.com>
2018-10-24 17:58:17 -0300

    Init #250

---
+++ bh/documentation/src/colors/Colors.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bh-grid bh-grid--column">
+    <div class="bh-grid__item">
+      <h2>Colors</h2>
+      <h3>Colors palette</h3>
+      <example
+        :component="ColorsPalette"
+        :code="colorsPaletteCode" />
+      <h3>Contrast</h3>
+      <example
+        :component="ContrastColors"
+        :code="contrastColorsCode" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Example from '@/Example';
+
+import ColorsPalette from './examples/ColorsPalette';
+import colorsPaletteCode from '!!raw-loader!./examples/ColorsPalette';
+import ContrastColors from './examples/Contrast';
+import contrastColorsCode from '!!raw-loader!./examples/Contrast';
+
+
+export default {
+  name: 'Colors',
+  components: {
+    Example,
+  },
+  data() {
+    return {
+      ColorsPalette,
+      colorsPaletteCode,
+      ContrastColors,
+      contrastColorsCode,
+    };
+  },
+};
+</script>
---
+++ bh/documentation/src/colors/examples/ColorsPalette.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="colors-palette">
+    <div class="background-color-primary">
+      <span>primary</span>
+    </div>
+    <div class="background-color-secondary">
+      <span>secondary</span>
+    </div>
+    <div class="background-color-danger">
+      <span>danger</span>
+    </div>
+    <div class="background-color-info">
+      <span>info</span>
+    </div>
+    <div class="background-color-warning">
+      <span>warning</span>
+    </div>
+    <div class="background-color-lighter-grey">
+      <span>lighter-grey</span>
+    </div>
+    <div class="background-color-grey">
+      <span>grey</span>
+    </div>
+    <div class="background-color-bolder-grey">
+      <span>bolder-grey</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorsPalette',
+};
+</script>
+
+<style lang="scss" scoped>
+$margin: .5rem;
+
+.colors-palette {
+  display: flex;
+  flex-wrap: wrap;
+  margin: -($margin);
+
+  & > * {
+    padding: 2rem;
+    margin: $margin;
+
+    span {
+      padding: .5rem;
+      background-color: rgba(255, 255, 255, .5);
+    }
+  }
+}
+</style>
