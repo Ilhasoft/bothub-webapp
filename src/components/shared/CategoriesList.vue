f66cffa063ddff936781b9303591fee734f2e631
Douglas Paz <eu@douglaspaz.com>
2018-10-29 16:31:46 -0300

    Improve CategoriesList in mobile

--- src/components/shared/CategoriesList.vue
+++ src/components/shared/CategoriesList.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="bh-grid bh-grid--inline">
-    <div
-      v-for="category in categories"
-      :key="category.id"
-      class="bh-grid__item">
-      <bh-button
-        :primary="val === category.id"
-        @click="select(category.id)">{{ category.name }}</bh-button>
+  <div>
+    <div class="bh-grid bh-grid--inline hide-mobile">
+      <div
+        v-for="category in categories"
+        :key="category.id"
+        class="bh-grid__item">
+        <bh-button
+          :primary="val === category.id"
+          @click="select(category.id)">{{ category.name }}</bh-button>
+      </div>
+    </div>
+    <div class="hide-desktop">
+      <div class="bh-grid">
+        <div class="bh-grid__item">
+          <bh-dropdown>
+            <bh-button
+              slot="trigger"
+              primary>
+              <span>{{ selectedCategoryName }}</span>
+              <bh-icon value="menu-down" />
+            </bh-button>
+            <bh-dropdown-item
+              v-for="category in categories"
+              :key="category.id"
+              @click="select(category.id)">{{ category.name }}</bh-dropdown-item>
+          </bh-dropdown>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +47,12 @@ export default {
     categories() {
       return [{ id: 0, name: 'All', active: this.current === 0 }]
         .concat(this.allCategories);
+    },
+    selectedCategoryName() {
+      return this.categories.reduce((current, category) => (
+        this.val > 0 && category.id === this.val
+          ? category.name
+          : current), this.categories[0].name);
     },
   },
   watch: {
