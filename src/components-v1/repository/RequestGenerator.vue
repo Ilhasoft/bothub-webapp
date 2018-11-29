2938be3320b0ef8e93072af8c9ecab05d4283ba2
Douglas Paz <eu@douglaspaz.com>
2018-11-29 15:33:40 -0300

    fix #299

--- src/components-v1/repository/RequestGenerator.vue
+++ src/components-v1/repository/RequestGenerator.vue
@@ -10,37 +10,19 @@
     </b-field>
     <b-tabs v-model="activeTab">
       <b-tab-item label="cURL">
-        <bh-highlighted-pre code-class="curl">curl -X POST \
-        -H 'Authorization: Bearer {{ authorizationUuid }}' \
-        -F 'language={{ language }}' \
-        -F "text={{ text_escaped.curl }}" \
-        https://nlp.bothub.it/parse/</bh-highlighted-pre>
+        <bh-highlighted-pre
+          :code="codes.curl"
+          code-class="bash" />
       </b-tab-item>
       <b-tab-item label="Python">
-        <bh-highlighted-pre code-class="python">import requests
-
-data = {
-  'language': '{{ language }}',
-  'text': '{{ text_escaped.python }}',
-}
-
-headers = { 'Authorization': 'Bearer {{ authorizationUuid }}' }
-r = requests.post('https://nlp.bothub.it/parse/', headers=headers, data=data)
-print(r.json())</bh-highlighted-pre>
+        <bh-highlighted-pre
+          :code="codes.python"
+          code-class="python" />
       </b-tab-item>
       <b-tab-item label="Javascript">
-        <bh-highlighted-pre code-class="javascript">var data = new FormData();
-data.append('language', '{{ language }}');
-data.append('text', '{{ text_escaped.javascript }}');
-
-var request = new XMLHttpRequest();
-request.onload = function () {
-  console.log(JSON.parse(request.response));
-};
-
-request.open('POST', 'https://nlp.bothub.it/parse/');
-request.setRequestHeader('Authorization', 'Bearer {{ authorizationUuid }}');
-request.send(data);</bh-highlighted-pre>
+        <bh-highlighted-pre
+          :code="codes.javascript"
+          code-class="javascript" />
       </b-tab-item>
     </b-tabs>
   </div>
@@ -78,9 +60,52 @@ export default {
   computed: {
     text_escaped() {
       return {
-        curl: this.text.replace(/"/g, '\\"'),
-        python: this.text.replace(/'/g, '\\\''),
-        javascript: this.text.replace(/'/g, '\\\''),
+        curl: this.text
+          .replace(/\\/g, '\\\\')
+          .replace(/"/g, '\\"'),
+        python: this.text
+          .replace(/\\/g, '\\\\')
+          .replace(/'/g, '\\\''),
+        javascript: this.text
+          .replace(/\\/g, '\\\\')
+          .replace(/'/g, '\\\''),
+      };
+    },
+    codes() {
+      return {
+        curl: [
+          'curl -X POST \\',
+          `  -H 'Authorization: Bearer ${this.authorizationUuid}' \\`,
+          `  -F 'language=${this.language}' \\`,
+          `  -F "text=${this.text_escaped.curl}" \\`,
+          '  https://nlp.bothub.it/parse/',
+        ].join('\n'),
+        python: [
+          'import requests',
+          '',
+          'data = {',
+          `  'language': '${this.language}',`,
+          `  'text': '${this.text_escaped.python}',`,
+          '}',
+          '',
+          `headers = { 'Authorization': 'Bearer ${this.authorizationUuid}' }`,
+          'r = requests.post(\'https://nlp.bothub.it/parse/\', headers=headers, data=data)',
+          'print(r.json())',
+        ].join('\n'),
+        javascript: [
+          'var data = new FormData();',
+          `data.append('language', '${this.language}');`,
+          `data.append('text', '${this.text_escaped.javascript}');`,
+          '',
+          'var request = new XMLHttpRequest();',
+          'request.onload = function () {',
+          '  console.log(JSON.parse(request.response));',
+          '};',
+          '',
+          'request.open(\'POST\', \'https://nlp.bothub.it/parse/\');',
+          `request.setRequestHeader('Authorization', 'Bearer ${this.authorizationUuid}');`,
+          'request.send(data);',
+        ].join('\n'),
       };
     },
   },
