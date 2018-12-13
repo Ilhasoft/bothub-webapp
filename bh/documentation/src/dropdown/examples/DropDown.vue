641c3f84bf2e621f57c98df3fed14eb6c9182a68
Victor Meneghini <vhmeneghini@gmail.com>
2018-12-13 11:40:03 -0300

    Change simple dropdown call

--- bh/documentation/src/dropdown/examples/DropDown.vue
+++ bh/documentation/src/dropdown/examples/DropDown.vue
@@ -3,24 +3,15 @@
     <div
       class="bh-grid__item item"
       shadow="shaded">
-      <h5>Drop Item to left side of dropdown</h5>
+      <h5>Choose your category</h5>
       <div>
         <bh-dropdown
           title="All categories"
           position="left">
-          <bh-dropdown-item>Edit</bh-dropdown-item>
-          <bh-dropdown-item>Remove</bh-dropdown-item>
-        </bh-dropdown>
-      </div>
-    </div>
-    <div
-      class="bh-grid__item item"
-      shadow="shaded">
-      <h5>Drop Item to right side of dropdown</h5>
-      <div>
-        <bh-dropdown title="All languages">
-          <bh-dropdown-item>Edit</bh-dropdown-item>
-          <bh-dropdown-item>Remove</bh-dropdown-item>
+          <bh-dropdown-item>Communication</bh-dropdown-item>
+          <bh-dropdown-item>Commerce</bh-dropdown-item>
+          <bh-dropdown-item>Tools</bh-dropdown-item>
+          <bh-dropdown-item>Finance</bh-dropdown-item>
         </bh-dropdown>
       </div>
     </div>
