b08c6bb240ef083c0e555fcce89d49ebe115a7ec
VictorMeneghini <vhmeneghini@gmail.com>
2018-12-18 10:38:39 -0300

    Add new environment variable

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,6 +4,8 @@ const packageDict = require('../package.json')
 module.exports = {
   NODE_ENV: '"production"',
   API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+  BOTHUB_NLP_BASE_URL: JSON.stringify('https://nlp.bothub.it'),
+  BOTHUB_WEBAPP_BASE_URL: JSON.stringify('https://bothub.it'),
   SUPPORTED_LANGUAGES: JSON.stringify(process.env.SUPPORTED_LANGUAGES || 'en|pt'),
   MAILCHIMP_LOGIN: JSON.stringify(process.env.MAILCHIMP_LOGIN),
   MAILCHIMP_DATACENTER: JSON.stringify(process.env.MAILCHIMP_DATACENTER),
