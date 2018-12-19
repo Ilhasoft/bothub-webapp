86fe3cb6f4bfb2ccee25179269597ab9404bc4ab
VictorMeneghini <vhmeneghini@gmail.com>
2018-12-19 17:02:24 -0300

    Add env enviroments on dev.env and update snapshot test

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -5,5 +5,6 @@ Vue.config.productionTip = false;
 
 process.env.SUPPORTED_LANGUAGES = 'en|pt';
 process.env.VERSION = '0.0.0';
+process.env.BOTHUB_NLP_BASE_URL = 'http://localhost:2657/';
 
 MockDate.set(1534341842684);
