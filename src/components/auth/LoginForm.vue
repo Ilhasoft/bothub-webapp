f3cc53dda14ce20207e3a25f9ab49f9888966f9e
VictorMeneghini <vhmeneghini@gmail.com>
2019-02-07 17:52:02 -0300

    New forms input schema

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,123 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <loading v-if="!formSchema" />
+    <form-generator
+      v-if="formSchema"
+      :drf-model-instance="drfLoginModel"
+      :schema="formSchema"
+      v-model="data"
+      :errors="errors"
+      class="field" />
+    <div
+      v-if="!hideForgotPassword"
+      class="field">
+      <div class="control has-text-right">
+        <a
+          ref="forgotPassword"
+          href="#forgot-password"
+          class="has-text-grey"
+          @click.prevent="forgotPasswordClick">Forgot password?</a>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control has-text-centered">
+        <button
+          :disabled="submitting"
+          type="submit"
+          class="button is-primary"
+        >Login</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import LoginModel from '@/models/login';
+import { getModel } from 'vue-mc-drf-model';
+import { mapActions } from 'vuex';
+import FormGenerator from '@/components/form-generator/FormGenerator';
+import Loading from '@/components-v1/shared/Loading';
+
+
+const components = {
+  FormGenerator,
+  Loading,
+};
+
+export default {
+  name: 'LoginForm',
+  components,
+  props: {
+    hideForgotPassword: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      formSchema: null,
+      data: {},
+      submitting: false,
+      errors: {},
+      drfLoginModel: {},
+    };
+  },
+  async mounted() {
+    this.formSchema = await this.getLoginSchema();
+    const Model = getModel(
+      this.formSchema,
+      LoginModel,
+    );
+
+    this.drfLoginModel = new Model(
+      {},
+      null,
+      {
+        validateOnChange: true,
+      },
+    );
+    console.log(this.drfLoginModel);
+
+    // console.log(this.formSchema);
+  },
+  methods: {
+    ...mapActions([
+      'getLoginSchema',
+      'login',
+    ]),
+    async onSubmit() {
+      const loginAttrs = this.drfLoginModel.defaults();
+
+      Object.keys(loginAttrs).forEach((attrName) => {
+        Object.keys(this.data).forEach((item) => {
+          if (attrName === item) {
+            this.drfLoginModel[attrName] = this.data[item];
+          }
+        });
+      });
+      console.log(loginAttrs);
+      this.drfLoginModel.getSaveData();
+      this.drfLoginModel.save();
+
+      // this.errors = {};
+      // this.submitting = true;
+      // try {
+      //   await this.login(this.data);
+      //   this.$emit('authenticated');
+      //   return true;
+      // } catch (error) {
+      //   const data = error.response && error.response.data;
+      //   if (data) {
+      //     this.errors = data;
+      //   }
+      //   this.submitting = false;
+      // }
+
+      // return false;
+    },
+    forgotPasswordClick() {
+      this.$emit('forgotPasswordClick');
+    },
+  },
+};
+</script>
---
+++ src/components/form-generator/FormGenerator.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <messages :msgs="msgs" />
+    <b-field
+      v-for="field in attrs"
+      v-show="field.type !== 'hidden'"
+      :key="field.name"
+      :label="field.description.label"
+      :type="field.errors && 'is-danger'"
+      :message="field.errors || field.helpText">
+      <component
+        :drf-erro="drfModelInstance.errors[field.name]"
+        :v-if="field.inputComponent"
+        :is="field.InputComponent"
+        v-bind="field.inputProps"
+        v-model="formData[field.name]"
+        :initial-data="initialData[field.name]"
+        @input="update()" />
+    </b-field>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+import Messages from '@/components-v1/shared/Messages';
+import StringInput from './inputs/StringInput';
+import ChoiceInput from './inputs/ChoiceInput';
+import BooleanInput from './inputs/BooleanInput';
+import MultipleChoice from './inputs/MultipleChoice';
+import TextInput from './inputs/TextInput';
+import EmailInput from './inputs/EmailInput';
+import PasswordInput from './inputs/PasswordInput';
+
+const relatedInputComponent = {
+  field: StringInput,
+  string: StringInput,
+  slug: StringInput,
+  choice: ChoiceInput,
+  boolean: BooleanInput,
+  'multiple choice': MultipleChoice,
+  text: TextInput,
+  email: EmailInput,
+  password: PasswordInput,
+  hidden: StringInput,
+};
+
+const components = {
+  Messages,
+};
+
+export default {
+  name: 'FormGenerator',
+  components,
+  props: {
+    drfModelInstance: {
+      type: Object,
+      default: () => ({}),
+    },
+    schema: {
+      required: true,
+      type: Object,
+    },
+    errors: {
+      type: Object,
+      default: () => ({}),
+    },
+    initialData: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      formData: {},
+    };
+  },
+  computed: {
+    attrs() {
+      return _.filter(
+        _.map(
+          this.drfModelInstance.attrsDescription,
+          (value, key) => ({
+            name: key,
+            description: value,
+            InputComponent: relatedInputComponent[value.type] || StringInput,
+          }),
+        ),
+        attr => !attr.description.read_only,
+      );
+    },
+    msgs() {
+      /* istanbul ignore next */
+      return (this.errors.non_field_errors
+        && this.errors.non_field_errors.map(text => ({ text, class: 'error' }))) || [];
+    },
+  },
+  mounted() {
+    this.update();
+  },
+  methods: {
+    update() {
+      this.$emit('input', this.formData);
+    },
+  },
+};
+</script>
