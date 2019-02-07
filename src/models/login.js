dea593975432adf2a6231471fa672f93dfb0a3f3
VictorMeneghini <vhmeneghini@gmail.com>
2019-02-07 17:57:35 -0300

    Create Login model

---
+++ src/models/login.js
@@ -0,0 +1,43 @@
+/* eslint-disable class-methods-use-this */
+import ModelBase from './base';
+import store from '@/store';
+
+
+class Login extends ModelBase {
+  defaults() {
+    return {
+      username: '',
+      password: '',
+    };
+  }
+
+  mutations() {
+    return {
+      email: String,
+      password: Number,
+    };
+  }
+
+  routes() {
+    return {
+      save: '/login/',
+    };
+  }
+
+  onSaveSuccess(response) {
+    super.onSaveSuccess(response);
+    store.dispatch(
+      'login',
+      {
+        username: this.username,
+        password: this.password,
+      },
+    );
+  }
+
+  onSaveFailure(response) {
+    super.onSaveFailure(response);
+  }
+}
+
+export default Login;
