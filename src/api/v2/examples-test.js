c46f9861dba832258f8f23e6bf5e4ececda251ac
VictorMeneghini <vhmeneghini@gmail.com>
2019-03-21 17:44:45 -0300

    redirect request to backend

--- src/api/v2/examples-test.js
+++ src/api/v2/examples-test.js
@@ -7,7 +7,7 @@ import utils from '@/api/utils';
 export default {
   new(repository, text, language, entities, intent) {
     return request.$http.post(
-      'http://localhost:3000/sentence/',
+      '/v2/validation/new/',
       {
         repository,
         text,
@@ -18,19 +18,19 @@ export default {
     );
   },
   all(repositoryUuid) {
-    const queryString = qs.stringify({ repository_uuid: repositoryUuid });
-    return new utils.List(`http://localhost:3000/sentence/?${queryString}`);
+    return this.search(repositoryUuid);
   },
   delete(exampleId) {
-    return request.$http.delete(`http://localhost:3000/allSentences/${exampleId}/`);
+    return request.$http.delete(`/v2/validation/${exampleId}/`);
   },
-  search() {
-    // query = {} / repositoryUuid -> add this to parameters
-    // const queryString = qs.stringify({
-    //   repository_uuid: repositoryUuid,
-    //   ...query,
-    // });
-    return new utils.List('http://localhost:3000/allSentences/');
+  search(repositoryUuid, query = {}) {
+    const queryString = qs.stringify({
+      repository_uuid: repositoryUuid,
+      ...query,
+    });
+    console.dir(queryString, 'aq');
+
+    return new utils.List(`/v2/validations/?${queryString}`);
   },
   get(id) {
     return request.$http.get(`/http://localhost:3000/sentence/${id}/`);
