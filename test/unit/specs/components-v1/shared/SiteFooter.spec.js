32eef0c9ce605a90cb2f84b6b95faf4f14fb9513
Samylla <samyllasr@gmail.com>
2019-04-23 17:31:41 -0300

    importing BH

--- test/unit/specs/components-v1/shared/SiteFooter.spec.js
+++ test/unit/specs/components-v1/shared/SiteFooter.spec.js
@@ -1,10 +1,12 @@
 import Buefy from 'buefy';
+import BH from 'bh';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import SiteFooter from '@/components-v1/shared/SiteFooter';
 
 
 const localVue = createLocalVue();
 localVue.use(Buefy);
+localVue.use(BH);
 
 describe('SiteFooter.vue', () => {
   let wrapper;
