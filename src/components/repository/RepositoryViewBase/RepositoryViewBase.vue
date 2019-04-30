e82f12980a9aabfcc43abe0269911af60c5c7a02
Samylla <samyllasr@gmail.com>
2019-04-30 10:09:28 -0300

    new navbar positioning

--- src/components/repository/RepositoryViewBase/RepositoryViewBase.vue
+++ src/components/repository/RepositoryViewBase/RepositoryViewBase.vue
@@ -12,9 +12,15 @@
       shadow="strong"
       class="rpstr-vw-bs__wrapper">
       <div class="rpstr-vw-bs__wrapper__header">
+        <router-link
+          class="rpstr-vw-bs__wrapper__header__logo"
+          to="/home">
+          <img
+            src="~@/assets/imgs/logo.svg"
+            alt="bothub">
+        </router-link>
         <repository-info
-          :repository="repository"
-          class="bh-grid__item rpstr-vw-bs__wrapper__header__info" />
+          :repository="repository" />
         <div
           :class="[
             'bh-grid__item',
@@ -23,12 +29,17 @@
         ]"/>
         <div
           v-if="authenticated && openMyProfile"
-          class="bh-grid__item--grow-0 rpstr-vw-bs__wrapper__header__avatar">
+          class="bh-grid__item--grow-0 rpstr-vw-bs__wrapper__header__options">
+          <user-avatar
+            :profile="myProfile"
+            size="medium"
+            class="rpstr-vw-bs__wrapper__header__options__avatar"/>
           <bh-dropdown position="left">
-            <user-avatar
+            <bh-icon
               slot="trigger"
-              :profile="myProfile"
-              size="medium" />
+              value="menu-down"
+              size="normal"
+              class="rpstr-vw-bs__wrapper__header__options__dropdown"/>
             <bh-dropdown-item @click="openMyProfile()">
               {{ myProfile.name || '...' }}
             </bh-dropdown-item>
@@ -47,7 +58,7 @@
         class="rpstr-vw-bs__wrapper__navigation hide-mobile" />
       <div
         class="rpstr-vw-bs__status-bar">
-        <div class="rpstr-vw-bs__status-bar bh-grid">
+        <div class="rpstr-vw-bs__status-bar rpstr-vw-bs__wrapper__content bh-grid">
           <side-bar-navigation
             :repository="repository"
             class="bh-grid__item rpstr-vw-bs__wrapper__header__info--mobile"/>
@@ -277,170 +288,187 @@ export default {
 };
 </script>
 
+
 <style lang="scss" scoped>
-@import '~bh/src/assets/scss/colors.scss';
-@import '~bh/src/assets/scss/variables.scss';
+  @import '~bh/src/assets/scss/colors.scss';
+  @import '~bh/src/assets/scss/variables.scss';
 
 
-.rpstr-vw-bs {
-  $navigation-height: 4rem;
-  $medium-screen: 1035px;
-  $header-height: (16rem + $navigation-height);
-  $wrapper-width: 100%;
-  background-color: $color-white;
-  height: 100%;
+  .rpstr-vw-bs {
+    $navigation-height: 4rem;
+    $medium-screen: 1035px;
+    $header-height: (16rem + $navigation-height);
+    $wrapper-width: 100%;
+    background-color: $color-white;
+    height: 100%;
 
 
-  &__status-bar {
-    background-color: $color-fake-white;
-    padding: 0 0rem;
-    justify-content: space-between;
-    flex-direction: row;
+    &__status-bar {
+      background-color: $color-fake-white;
+      justify-content: space-between;
+      flex-direction: row;
+      padding: 0;
 
-    :nth-child(3n) {
-      // margin-right: 8px;
-    }
+      :nth-child(3n) {
+        // margin-right: 8px;
+      }
 
       @media screen and (max-width: $medium-screen) {
         justify-content: space-around;
       }
 
-    &__repo-info {
-       @media screen and (max-width: $medium-screen) {
+      &__repo-info {
+        @media screen and (max-width: $medium-screen) {
+          flex-grow: 0;
+          flex-direction: row;
+          justify-content: flex-end;
+          padding: 0px;
+        }
+      }
+
+      &__icons-align {
+        display: flex;
+        align-items: center;
+        justify-content: center;
         flex-grow: 0;
-        justify-content: flex-end;
-        flex-direction: row;
-        padding: 0px;
-      }
-    }
 
-    &__icons-align {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-grow: 0;
-
-      &--size {
-        @media screen and (max-width: $medium-screen) {
-          height: 1.5rem;
-          width: 1.5rem;
-        }
-      }
-    }
-
-    &__text-information {
-      margin: 0 .25rem;
-    }
-  }
-
-  &__wrapper {
-    &__header {
-      position: relative;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      margin: 0px;
-
-      @media screen and (max-width: $medium-screen) {
-        display: none;
-      }
-
-      &__avatar {
-        border: .120rem solid $color-primary;
-        border-radius: 50%;
-        margin-right: 3rem;
-
-        @media screen and (max-width: $medium-screen) {
-          display: none;
-        }
-      }
-
-      &__info {
-        margin-left: 2rem;
-        flex-direction: row;
-
-        @media screen and (max-width: $medium-screen) {
-          display: none;
-        }
-
-
-        &--mobile {
-          display: none;
+        &--size {
           @media screen and (max-width: $medium-screen) {
-            display: inline;
-            flex-grow: 1;
-            margin:0px;
+            height: 1.5rem;
+            width: 1.5rem;
           }
         }
       }
 
-      &__navigation {
-        position: absolute;
-        bottom: 0rem;
-        left: 0;
-        width: 100%;
+      &__text-information {
+        margin: 0 .25rem;
+      }
+    }
+
+    &__wrapper {
+      &__header {
+        position: relative;
+        display: grid;
+        grid-template-columns: 10% 1fr 10%;
+        margin: 1rem 1.5rem;
 
         @media screen and (max-width: $medium-screen) {
           display: none;
         }
-      }
 
-      &__mobile-navigation {
-        display: none;
+        &__logo {
+          align-self: center;
+        }
+
+        &__options {
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+
+          &__avatar {
+            text-align: right;
+            border: .120rem solid $color-primary;
+            border-radius: 50%;
+            cursor: auto;
+
+            @media screen and (max-width: $medium-screen) {
+              display: none;
+            }
+          }
+
+          &__dropdown {
+            cursor: pointer;
+          }
+        }
+
+        &__info {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          margin-left: 2rem;
+
+          @media screen and (max-width: $medium-screen) {
+            display: none;
+          }
+
+
+          &--mobile {
+            display: none;
+            @media screen and (max-width: $medium-screen) {
+              display: inline;
+              flex-grow: 1;
+              margin:0px;
+            }
+          }
+        }
+
+        &__navigation {
+          position: absolute;
+          bottom: 0rem;
+          left: 0;
+          width: 100%;
+
+          @media screen and (max-width: $medium-screen) {
+            display: none;
+          }
+        }
+
+        &__mobile-navigation {
+          display: none;
+
+          @media screen and (max-width: $mobile-width) {
+            display: block;
+          }
+        }
 
         @media screen and (max-width: $mobile-width) {
-          display: block;
+          min-height: auto;
+          padding: 1rem;
         }
       }
 
-      @media screen and (max-width: $mobile-width) {
-        min-height: auto;
-        padding: 1rem;
+      &__navigation {
+        border-top: .120rem solid whitesmoke
+      }
+
+      &__content {
+        max-width: 80%;
+        margin: 0 auto;
+      }
+
+      @media screen and (max-width: $wrapper-width) {
+        border-radius: 0;
       }
     }
 
-    &__navigation {
-      border-top: .120rem solid whitesmoke
+    &__error,
+    &__loading {
+      text-align: center;
     }
 
-    &__content {
-      max-width: 80%;
-      margin: 0 auto;
+    &__error {
+      padding: .5rem;
+      h1 {
+        font-size: 3rem;
+        line-height: 4rem;
+        font-weight: $font-weight-bolder;
+        color: black;
+        opacity: .5;
+      }
     }
 
     @media screen and (max-width: $wrapper-width) {
-      border-radius: 0;
+      margin: 1rem 0;
+    }
+
+    &__footer {
+      margin-top: 60vh;
     }
   }
 
-  &__error,
-  &__loading {
-    text-align: center;
-  }
-
-  &__error {
-    padding: .5rem;
-    h1 {
-      font-size: 3rem;
-      line-height: 4rem;
-      font-weight: $font-weight-bolder;
-      color: black;
-      opacity: .5;
+  .hide-mobile {
+    @media screen and (max-width: 1035px) {
+      display: none;
     }
   }
-
-  @media screen and (max-width: $wrapper-width) {
-    margin: 1rem 0;
-  }
-
-  &__footer {
-    margin-top: 60vh;
-  }
-}
-
-.hide-mobile {
-  @media screen and (max-width: 1035px) {
-    display: none;
-  }
-}
 </style>
