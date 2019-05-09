8d4fc9057253a7bc3e441c46d3931f88d9246538
Samylla <samyllasr@gmail.com>
2019-05-08 22:03:05 -0300

    :recycle: Add tests for FilterEvaluateResultExample component

--- test/unit/specs/components/repository/repository-evaluate/results/FilterEvaluateResultExample.spec.js
+++ test/unit/specs/components/repository/repository-evaluate/results/FilterEvaluateResultExample.spec.js
@@ -1,14 +1,18 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import FilterEvaluateResultExample from '@/components/repository/repository-evaluate/results/FilterEvaluateResultExample';
 import BH from 'bh';
-
+import sinon from 'sinon';
 
 const localVue = createLocalVue();
 localVue.use(BH);
 
 describe('FilterEvaluateResultExample.vue', () => {
   let wrapper;
+  let clock;
+
   beforeEach(() => {
+    clock = sinon.useFakeTimers();
+
     wrapper = shallowMount(FilterEvaluateResultExample, {
       localVue,
       propsData: {
@@ -20,7 +24,53 @@ describe('FilterEvaluateResultExample.vue', () => {
     });
   });
 
+  afterEach(() => {
+    clock.restore();
+  });
+
   it('renders', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('emits queryStringFormated event when text changes', () => {
+    wrapper.setData({
+      text: 'Something',
+    });
+
+    expect(wrapper.emitted('queryStringFormated')).toBeDefined();
+    expect(wrapper.emitted('queryStringFormated').length).toBe(1);
+    expect(wrapper.emitted('queryStringFormated')[0]).toEqual([{ search: 'Something' }]);
+  });
+
+  it('emits queryStringFormated event when intent changes after debounce time', () => {
+    wrapper.setData({
+      intent: 'Something',
+    });
+
+    clock.tick(499);
+
+    expect(wrapper.emitted('queryStringFormated')).toBeUndefined();
+
+    clock.tick(1);
+
+    expect(wrapper.emitted('queryStringFormated')).toBeDefined();
+    expect(wrapper.emitted('queryStringFormated').length).toBe(1);
+    expect(wrapper.emitted('queryStringFormated')[0]).toEqual([{ intent: 'Something' }]);
+  });
+
+  it('emits queryStringFormated event when entity changes after debounce time', () => {
+    wrapper.setData({
+      entity: 'Something',
+    });
+
+    clock.tick(499);
+
+    expect(wrapper.emitted('queryStringFormated')).toBeUndefined();
+
+    clock.tick(1);
+
+    expect(wrapper.emitted('queryStringFormated')).toBeDefined();
+    expect(wrapper.emitted('queryStringFormated').length).toBe(1);
+    expect(wrapper.emitted('queryStringFormated')[0]).toEqual([{ entity: 'Something' }]);
   });
 });
