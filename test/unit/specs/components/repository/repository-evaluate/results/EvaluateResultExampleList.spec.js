28e9c2c2ec7462cca87aec038d1204156d680754
Samylla <samyllasr@gmail.com>
2019-05-08 22:59:45 -0300

    :white_check_mark: updating EvaluateResultExampleList component tests

--- test/unit/specs/components/repository/repository-evaluate/results/EvaluateResultExampleList.spec.js
+++ test/unit/specs/components/repository/repository-evaluate/results/EvaluateResultExampleList.spec.js
@@ -2,7 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils';
 import EvaluateResultExampleList from '@/components/repository/repository-evaluate/results/EvaluateResultExampleList';
 import BH from 'bh';
 import store from '@/store';
-
+import sinon from 'sinon';
 
 const localVue = createLocalVue();
 localVue.use(BH);
@@ -24,5 +24,25 @@ describe('EvaluateResultExampleList.vue', () => {
 
   test('renders correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('updateList is called when mounted', () => {
+    const updateListStub = sinon.stub();
+
+    wrapper = shallowMount(EvaluateResultExampleList, {
+      localVue,
+      propsData: {
+        repository: {
+          uuid: '8511fd26-a3bc-4f74-9af1-176abca5401d',
+        },
+        id: 2,
+      },
+      store,
+      methods: {
+        updateList: updateListStub,
+      },
+    });
+
+    expect(updateListStub.called).toBeTruthy();
   });
 });
