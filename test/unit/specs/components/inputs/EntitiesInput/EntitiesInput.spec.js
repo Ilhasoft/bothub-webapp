b8bd2a15b7ebfff04b03592e9ea13df6327fa09d
Samylla <samyllasr@gmail.com>
2019-06-16 21:06:20 -0300

    :white_check_mark: Adds the test cases of new entity input component

--- test/unit/specs/components/inputs/EntitiesInput/EntitiesInput.spec.js
+++ test/unit/specs/components/inputs/EntitiesInput/EntitiesInput.spec.js
@@ -19,83 +19,13 @@ describe('EntitiesInput.vue', () => {
     });
   });
 
-  test('renders correctly', () => {
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  describe('newEntity emit new event', () => {
-    let newEntity;
-    const entity = {
-      start: 11,
-      end: 18,
-      entity: 'name',
-    };
-
-    beforeEach(() => {
-      newEntity = wrapper.find({ ref: 'newEntity' });
-      newEntity.vm.$emit('new', entity);
-    });
-
-    test('renders correctly', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    test('new event emitted by newEntity', () => {
-      expect(newEntity.emitted('new')).toBeDefined();
-    });
-
-    test('entity added to entities', () => {
-      expect(wrapper.vm.entities).toHaveLength(1);
-    });
-
-    test('input event emitted', () => {
-      expect(wrapper.emitted('input')).toBeDefined();
-    });
-
-    describe('change text to "the name is douglas"', () => {
-      beforeEach(() => {
-        wrapper.setProps({ text: 'the name is douglas' });
-      });
-
-      test('update entity position', () => {
-        const e = wrapper.vm.entities[0];
-        expect(e.start).toBe(12);
-        expect(e.end).toBe(19);
-      });
-    });
-
-    describe('change text to "my name is"', () => {
-      beforeEach(() => {
-        wrapper.setProps({ text: 'the name is' });
-      });
-
-      test('renders correctly', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      test('entity deleted', () => {
-        expect(wrapper.vm.entities).toHaveLength(0);
-      });
-    });
-
-    describe('remove entity', () => {
-      beforeEach(() => {
-        wrapper.vm.removeEntity(entity);
-      });
-
-      test('entity deleted', () => {
-        expect(wrapper.vm.entities).toHaveLength(0);
-      });
-    });
-
-    describe('edit entity', () => {
-      beforeEach(async () => {
-        await wrapper.vm.editEntity(entity);
-      });
-
-      test('entity deleted', () => {
-        expect(wrapper.vm.entities).toHaveLength(0);
-      });
-    });
-  });
+  xit('add entity button is disabled when nothing is selected');
+  xit('add entity button enables when some text is selected');
+  xit('adds an entity');
+  xit('adds label to an entity');
+  xit('shows existing label of an entity');
+  xit('removes label from an entity');
+  xit('updates label of an entity');
+  xit('removes an entity');
+  xit('renames an entity');
 });
