2cdc89481c81395371ca9b67cd48f8ee99298a3c
Samylla <samyllasr@gmail.com>
2019-06-16 21:28:17 -0300

    :white_check_mark: Implementing test for when the add entity button is enabled

--- test/unit/specs/components/inputs/EntitiesInput/EntitiesInput.spec.js
+++ test/unit/specs/components/inputs/EntitiesInput/EntitiesInput.spec.js
@@ -23,11 +23,27 @@ describe('EntitiesInput.vue', () => {
 
   it('add entity button is disabled when nothing is selected', () => {
     const btn = findAddEntityButton();
+
     expect(btn.exists()).toBe(true);
     expect(btn.text()).toBe('Add entity');
     expect(btn.props('disabled')).toBe(true);
   });
-  xit('add entity button enables when some text is selected');
+
+  it('add entity button enables when some text is selected', () => {
+    wrapper.setProps({
+      text: '000aaaa000',
+      textSelected: {
+        start: 3,
+        end: 7,
+      },
+    });
+
+    const btn = findAddEntityButton();
+
+    expect(btn.text()).toBe('Add entity for "aaaa"');
+    expect(btn.props('disabled')).toBe(false);
+  });
+
   xit('adds an entity');
   xit('adds label to an entity');
   xit('shows existing label of an entity');
