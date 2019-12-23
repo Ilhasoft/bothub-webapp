0a85b912f94b95f8084b0f2f99a1e61ad89ff487
VictorMeneghini <vhmeneghini@gmail.com>
2019-12-23 16:35:54 -0300

    Create new component for wrapper layout
    Create a new component to manager all responsability about repository interface

---
+++ src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dashboard-layout">
+    <side-bar @colapse="colapseHandle()" />
+
+    <div :class="colapse ? 'dashboard-layout__main-panel': 'dashboard-layout__main-panel--colapsed'">
+      <router-view />
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+import SideBar from '@/components/repository/sidebar/SideBar.vue';
+// import ContentFooter from "./ContentFooter.vue";
+// import DashboardContent from "./Content.vue";
+// import MobileMenu from "./MobileMenu";
+export default {
+  components: {
+    SideBar,
+    // ContentFooter,
+    // DashboardContent,
+    // MobileMenu
+  },
+  data() {
+    return {
+      linkHeight: 65,
+      activeLinkIndex: 0,
+      windowWidth: 0,
+      isWindows: false,
+      hasAutoHeight: false,
+      links: [],
+      isActive: true,
+      colapse: true,
+    };
+  },
+  methods: {
+    colapseHandle() {
+      this.colapse = !this.colapse;
+    },
+  },
+};
+</script>
+<style lang="scss">
+.dashboard-layout {
+
+  &__main-panel {
+    width: calc( 100% - 260px);
+    position: relative;
+    float: right;
+
+    &--colapsed {
+       position: relative;
+       float: right;
+       width: calc( 100% - 80px);
+    }
+  }
+}
+</style>
