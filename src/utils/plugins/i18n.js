408b332d013661ab0ef9c4c23cef283a175903c9
Daniel Yohan <dyohan9@gmail.com>
2020-01-15 17:17:11 -0300

    Updated i18n

--- src/utils/plugins/i18n.js
+++ src/utils/plugins/i18n.js
@@ -2,13 +2,11 @@ import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
 import en from '@/locales/en.json';
-import ptbr from '@/locales/pt_br.json';
 
 Vue.use(VueI18n);
 
 const languages = {
   'en-US': en,
-  'pt-BR': ptbr,
 };
 
 const i18n = new VueI18n({
