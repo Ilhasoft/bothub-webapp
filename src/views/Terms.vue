e5a6d9266c7b205b3549ed8b870d3be482e76987
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-03-17 17:25:41 -0300

    fixed lint errors

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,5 +1,5 @@
 <template>
-
+<!-- eslint-disable -->
   <div class="termsStyle">
     <p class="termsStyle__title">TERMS OF USE BOTHUB</p>
     <p class="termsStyle__section">1. ACCEPTANCE</p>
@@ -36,28 +36,28 @@
     <p class="termsStyle__content">
       <span class="termsStyle__content__bold">CLIENTS:</span> It is the customers or interviewed by the user, individual or legal, who received messages through the BotHub platform.
     </p>
-     <p class="termsStyle__content">
+    <p class="termsStyle__content">
       <span class="termsStyle__content__bold">INTELLIGENCE:</span> It is the collection of data that is stored in BotHub in order to train the computer to understand the human language based on specific context.
     </p>
-     <p class="termsStyle__content">
+    <p class="termsStyle__content">
       <span class="termsStyle__content__bold">TRAIN:</span>  It is the process of adding sentences to BotHub that teaches the computer to understand the human language.
     </p>
-     <p class="termsStyle__content">
+    <p class="termsStyle__content">
       <span class="termsStyle__content__bold">PREDICT:</span> After a training process it enables the computer to make predictions on text.
     </p>
     <hr>
 
-     <p class="termsStyle__section">3. SERVICE</p>
+    <p class="termsStyle__section">3. SERVICE</p>
 
-      <p class="termsStyle__content">
+    <p class="termsStyle__content">
       BotHub is a platform that allows you to create Artificial Intelligence models in an easy and scalable way.
-      </p>
-
-       <p class="termsStyle__content">
-     Once you've created an Intelligence, you'll be able to train it with text data that in the end will be delivered as predictions for 3rd-party platforms to make predictions by using the BotHub API.
     </p>
 
-       <p class="termsStyle__content">
+    <p class="termsStyle__content">
+      Once you've created an Intelligence, you'll be able to train it with text data that in the end will be delivered as predictions for 3rd-party platforms to make predictions by using the BotHub API.
+    </p>
+
+    <p class="termsStyle__content">
       BotHub, as it appears in the definitions, consists of a ILHASOFT OpenSource platform that's hosted in the company cloud servers in accordance with the privacy policy available in <a href="/terms">https://bothub.it/terms.</a>
     </p>
 
@@ -70,7 +70,7 @@
     </p>
 
     <p class="termsStyle__content">
-     To get started, the first step is to choose an existing Intelligence or to create a new one, depending on what you need you'll follow both paths or just one of them. If you choose an existing intelligence you can just see how it is working, test it, and open the integrations page to get instructions in how to use it, however, if you decide to create a new one, you simply input some information about it and start the training process, once you are confident enough about your intelligence you can open the integrations page and use start using it.
+      To get started, the first step is to choose an existing Intelligence or to create a new one, depending on what you need you'll follow both paths or just one of them. If you choose an existing intelligence you can just see how it is working, test it, and open the integrations page to get instructions in how to use it, however, if you decide to create a new one, you simply input some information about it and start the training process, once you are confident enough about your intelligence you can open the integrations page and use start using it.
     </p>
 
     <hr>
@@ -98,23 +98,23 @@
     <p class="termsStyle__section">7. LIMITATIONS OF LIABILITY</p>
 
     <p class="termsStyle__content">
-     You are solely responsible for the use of BotHub and you must abide by the rules of these Terms of Use as well as the applicable laws of BotHub.
+      You are solely responsible for the use of BotHub and you must abide by the rules of these Terms of Use as well as the applicable laws of BotHub.
     </p>
 
     <p class="termsStyle__content">
-    In no event will Ilhasoft, its controller, its affiliates, partners or employees be liable for any direct or indirect damages resulting from or in connection with access, use or inability to access or use BotHub.
-    </p>
-
-     <p class="termsStyle__content">
-    IN VIEW OF THE CHARACTERISTICS INHERENT OF THE INTERNET ENVIRONMENT, ILHASOFT IS NOT RESPONSIBLE FOR CONNECTION INTERRUPTIONS OR SUSPENSIONS, COMPUTER TRANSMISSIONS INCOMPLETE OR THAT FAIL, AND FOR TECHNICAL FAILURE OF ANY KIND INCLUDING, BUT NOT LIMITED TO, THE MALFUNCTION ELECTRONICS OF ANY NETWORK, HARDWARE OR SOFTWARE. THE UNAVAILABILITY OF ACCESS TO THE INTERNET OR BOTHUB, AS WELL AS AN INCORRECT OR INCOMPLETE INFORMATION ABOUT BOTHUB AND ANY HUMAN, TECHNICAL OR OTHER FAILURE IN THE PROCESSING OF BOTHUB INFORMATION, SHALL NOT BE REGARDED AS BOTHUB'S LIABILITY. BOTHUB DISCLAIMS ANY RESPONSIBILITY FOR SUCH FACTS AND/OR ACTS.
-    </p>
-
-     <p class="termsStyle__content">
-    It is your responsibility to keep the environment of your device (computer, mobile phone, tablet, etc.) safe, with the use of available tools, such as antivirus, firewall, among others, to help prevent electronic risks.
+      In no event will Ilhasoft, its controller, its affiliates, partners or employees be liable for any direct or indirect damages resulting from or in connection with access, use or inability to access or use BotHub.
     </p>
 
     <p class="termsStyle__content">
-    BotHub may contain links to third party sites and applications. This does not imply in any way that BotHub endorses, verifies, warrants or has any connection with the owners of such sites or applications and is not responsible for its content, accuracy, policies, practices or opinions. BotHub recommends that you read the terms of use and privacy policies of each third-party site or service that you visit or use.
+      IN VIEW OF THE CHARACTERISTICS INHERENT OF THE INTERNET ENVIRONMENT, ILHASOFT IS NOT RESPONSIBLE FOR CONNECTION INTERRUPTIONS OR SUSPENSIONS, COMPUTER TRANSMISSIONS INCOMPLETE OR THAT FAIL, AND FOR TECHNICAL FAILURE OF ANY KIND INCLUDING, BUT NOT LIMITED TO, THE MALFUNCTION ELECTRONICS OF ANY NETWORK, HARDWARE OR SOFTWARE. THE UNAVAILABILITY OF ACCESS TO THE INTERNET OR BOTHUB, AS WELL AS AN INCORRECT OR INCOMPLETE INFORMATION ABOUT BOTHUB AND ANY HUMAN, TECHNICAL OR OTHER FAILURE IN THE PROCESSING OF BOTHUB INFORMATION, SHALL NOT BE REGARDED AS BOTHUB'S LIABILITY. BOTHUB DISCLAIMS ANY RESPONSIBILITY FOR SUCH FACTS AND/OR ACTS.
+    </p>
+
+    <p class="termsStyle__content">
+      It is your responsibility to keep the environment of your device (computer, mobile phone, tablet, etc.) safe, with the use of available tools, such as antivirus, firewall, among others, to help prevent electronic risks.
+    </p>
+
+    <p class="termsStyle__content">
+      BotHub may contain links to third party sites and applications. This does not imply in any way that BotHub endorses, verifies, warrants or has any connection with the owners of such sites or applications and is not responsible for its content, accuracy, policies, practices or opinions. BotHub recommends that you read the terms of use and privacy policies of each third-party site or service that you visit or use.
     </p>
 
     <hr>
@@ -122,47 +122,47 @@
     <p class="termsStyle__section">8. CONTENT AVAILABILITY</p>
 
     <p class="termsStyle__content">
-    BotHub may, in its sole discretion, allow you or any other user to submit, press, publish or provide, in the application, content or text information, audio or video ( "User Content").
+      BotHub may, in its sole discretion, allow you or any other user to submit, press, publish or provide, in the application, content or text information, audio or video ( "User Content").
     </p>
 
     <p class="termsStyle__content">
-    Any User Content that is defamatory, libelous, abusive, violent, pornographic, obscene, offensive or unlawful, as determined by Ilhasoft, in its sole discretion, including proprietary information belonging to other persons or companies (eg. author, trademarks) without the express permission of the owner of such rights, whose infringement will not be the responsibility of Ilhasoft.
+      Any User Content that is defamatory, libelous, abusive, violent, pornographic, obscene, offensive or unlawful, as determined by Ilhasoft, in its sole discretion, including proprietary information belonging to other persons or companies (eg. author, trademarks) without the express permission of the owner of such rights, whose infringement will not be the responsibility of Ilhasoft.
     </p>
 
-     <p class="termsStyle__content">
-   Any User Content provided by you remains under your ownership. 
+    <p class="termsStyle__content">
+      Any User Content provided by you remains under your ownership.
     </p>
 
     <hr>
 
     <p class="termsStyle__section">9. LEGAL RESTRICTIONS</p>
 
-      <p class="termsStyle__content">
+    <p class="termsStyle__content">
       <span class="termsStyle__content__bold">YOUR CONTENT: </span> you are the sole and exclusive responsible for the content of the texts made available in the intelligences, making sure that the contents will not violate and will not cause undue exposure or embarrassment of the end users, also responding by their content in court or out of it, and exempting Ilhasoft unconditionally from any joint or subsidiary liability for any Claims, given the intermediate nature of the service that Ilhasoft is providing to you through the use of the BotHub platform. You must also ensure that your Content, your submissions and your use - and your use by end users - including your offers and services, do not violate any applicable Policies or Laws. You are fully responsible for the development, content, operation, maintenance and use of your Content, submissions and offers and services made available. You may not, under any circumstances, provide Content that: (a) is false or misleading, or offers Content other than the ones that the user will engage in, attempting in any way to deceive / induce and / or offend the law, morals or commercial ethics; (b) invade the privacy of others or impair their image or honor in any way, or further promote any form of racism or against minority groups, or preach intolerance, prejudice; religious or political fanaticism or discrimination against groups of people or ethnic groups, as well as containing obscene content, such as pedophilia and other crimes of a sexual nature, are in any way prohibited or not recommended for a given age group, or defend or apologize to drugs and drug trafficking, narcotics, cigarettes, alcoholic beverages or illegal games, and (c) Content that violates third party rights, including but not limited to copyright, and/or the creation and dissemination of Content that you are not authorized to do so and offer Illegal and/or pirated Content, infringing the copyrights and property of third parties.
-      </p>
+    </p>
 
-      <p class="termsStyle__content">
+    <p class="termsStyle__content">
       <span class="termsStyle__content__bold">YOUR CLIENT: </span> the end users of the service and/or products offered by you through the BotHub platform, have a unique and exclusive relationship with you, therefore, you are also responsible for the use of your Content and offers used by your end user client. Therefore, you will exempt Ilhasoft and will be responsible for all end users to observe the applicable Policies and/or Laws. Furthermore, if you become aware of any breach of your obligations described in this Term of Use, caused by an end user of your account, you must immediately suspend access to your Content, the offers and services provided, and Ilhasoft will temporarily suspend the access to the BotHub platform, until the problem is solved.
-      </p>
+    </p>
 
     <hr>
 
     <p class="termsStyle__section">10. COMPLAINTS ABOUT COPYRIGHT INFRINGEMENT</p>
 
     <p class="termsStyle__content">
-    Allegations of copyright infringement of any content available on BotHub should be sent via e-mail to contato@ilhasoft.com.br or to the following address: Comerc José Pontes de Magalhães Street, n. 70, 803, Jatiúca, city Maceió, State of Alagoas, CEP 57036-250.
+      Allegations of copyright infringement of any content available on BotHub should be sent via e-mail to contato@ilhasoft.com.br or to the following address: Comerc José Pontes de Magalhães Street, n. 70, 803, Jatiúca, city Maceió, State of Alagoas, CEP 57036-250.
     </p>
 
     <hr>
 
-     <p class="termsStyle__section">11. GENERAL INFORMATION</p>
+    <p class="termsStyle__section">11. GENERAL INFORMATION</p>
 
     <p class="termsStyle__content">
-    These Terms of Use are governed by the laws of the Federative Republic of Brazil. Any doubts or situations not foreseen in these Terms of Use will first be resolved by BotHub and, if they persist, should be resolved by the Forum of the District of Maceió/AL, to the exclusion of any other, however privileged it may or may become.
+      These Terms of Use are governed by the laws of the Federative Republic of Brazil. Any doubts or situations not foreseen in these Terms of Use will first be resolved by BotHub and, if they persist, should be resolved by the Forum of the District of Maceió/AL, to the exclusion of any other, however privileged it may or may become.
     </p>
 
-     <p class="termsStyle__content">
-    If you have any questions, comments or suggestions, please contact us by e-mail contato@ilhasoft.com.br, or by the address Comerc. José Pontes de Magalhães Street, n. 70, 803, Jatiúca, city of Maceió, State of Alagoas, CEP 57036-250. 
+    <p class="termsStyle__content">
+      If you have any questions, comments or suggestions, please contact us by e-mail contato@ilhasoft.com.br, or by the address Comerc. José Pontes de Magalhães Street, n. 70, 803, Jatiúca, city of Maceió, State of Alagoas, CEP 57036-250.
     </p>
     <hr>
   </div>
