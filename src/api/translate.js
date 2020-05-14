5650649a8ed998361ecf1f7048adb31ad8b05e15
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-05-14 18:31:43 -0300

    Added import and export translate

--- src/api/translate.js
+++ src/api/translate.js
@@ -1,6 +1,6 @@
+import qs from 'query-string';
 import request from './request';
 import utils from './utils';
-
 
 export default {
   new(exampleId, language, text, entities) {
@@ -19,5 +19,25 @@ export default {
   },
   delete(translationId) {
     return request.$http.delete(`/v2/repository/translation/${translationId}/`);
+  },
+  exportTranslate(repositoryUuid, versionUUID, fromLanguage, toLanguagem, statusTranslation) {
+    const queryString = qs.stringify({
+      of_the_language: fromLanguage,
+      for_the_language: toLanguagem,
+      with_translation: statusTranslation,
+    });
+    return request.$http.get(`/v2/repository/translation-export/${repositoryUuid}/${versionUUID}/?${queryString}`, {
+      responseType: 'arraybuffer',
+    });
+  },
+  importTranslate(repositoryUuid, versionUUID, formData) {
+    return request.$http.put(`/v2/repository/translation-export/${repositoryUuid}/${versionUUID}/`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        responseType: 'arraybuffer',
+      });
   },
 };
