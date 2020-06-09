4a8263ac92ca14c735dc513b595a8cc39909c569
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-06-09 13:13:34 -0300

    Removed action not used

--- src/components/repository/EntitiesList.vue
+++ src/components/repository/EntitiesList.vue
@@ -96,7 +96,6 @@ export default {
     ...mapActions([
       'editEntityName',
       'setUpdateRepository',
-      'getAllEntities',
     ]),
     getEntityClass(entity) {
       const color = getEntityColor(
