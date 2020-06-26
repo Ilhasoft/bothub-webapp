f609758876a4cf0b1647952b4e313b80275a4cce
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-06-26 12:06:16 -0300

    Added translation

--- src/components/payment/PaymentInfoCard.vue
+++ src/components/payment/PaymentInfoCard.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="payment-info-card__wrapper">
     <div class="payment-info-card">
+      <h1> {{ $t('webapp.payment.info.title') }} </h1>
+      <p> {{ $t('webapp.payment.info.subtitle') }} </p>
       <form>
         <form-generator
           :schema="schema"
           :show-labels="false"/>
-        <b-button type="is-primary"> Back </b-button>
-        <b-button type="is-primary"> Save </b-button>
+        <b-button type="is-primary"> {{ $t('webapp.payment.info.back') }} </b-button>
+        <b-button type="is-primary"> {{ $t('webapp.payment.info.save') }} </b-button>
       </form>
     </div>
   </div>
@@ -57,11 +59,16 @@ export default {
 @import '~@/assets/scss/colors.scss';
 
   .payment-info-card {
-    padding: 0 4.7rem 3.5rem 4.7rem;
+    padding: 2rem 4.7rem 3.5rem 4.7rem;
     background-color: $color-white;
     margin: 0 1rem;
     box-shadow: 0px 3px 6px #00000029;
     max-width: 31rem;
+
+    h1 {
+      font-size: 2.5rem;
+      color: $color-primary;
+    }
 
     &__wrapper {
       display: flex;
