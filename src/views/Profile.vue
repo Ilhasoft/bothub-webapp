5d452426bc47e67bac0d84a041d0e564235204f9
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-07-03 13:44:17 -0300

    Added intelligences

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -19,12 +19,37 @@
         :selected.sync="selected"
         class="profile__tabs" />
     </div>
-    <div
-      v-show="selected==0"
-      class="profile__content">
-      <div>
+    <div>
+      <div
+        v-show="selected==0"
+        class="profile__content">
         <h1> Personal information </h1>
         <edit-profile-form />
+      </div>
+      <div v-show="selected==1">
+        <h1> My Intelligences </h1>
+        <paginated-list
+          v-if="repositoryList"
+          :item-component="repositoryItemElem"
+          :per-page="repositoriesLimit"
+          :list="repositoryList"
+          class="profile__repositories__cards" />
+
+        <h1> Contribution intelligences </h1>
+        <paginated-list
+          v-if="repositoryList"
+          :item-component="repositoryItemElem"
+          :per-page="repositoriesLimit"
+          :list="repositoryList"
+          class="profile__repositories__cards" />
+
+        <h1> Use intelligences </h1>
+        <paginated-list
+          v-if="repositoryList"
+          :item-component="repositoryItemElem"
+          :per-page="repositoriesLimit"
+          :list="repositoryList"
+          class="profile__repositories__cards" />
       </div>
     </div>
   </layout>
@@ -34,8 +59,10 @@
 import Layout from '@/components/shared/Layout';
 import UserAvatar from '@/components/user/UserAvatar';
 import EditProfileForm from '@/components/user/EditProfileForm';
+import RepositoryCard from '@/components/repository/RepositoryCard';
 import TabSelect from '@/components/shared/TabSelect';
-import { mapGetters } from 'vuex';
+import PaginatedList from '@/components/shared/PaginatedList';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Profile',
@@ -44,11 +71,15 @@ export default {
     UserAvatar,
     EditProfileForm,
     TabSelect,
+    PaginatedList,
   },
   data() {
     return {
       selected: 0,
+      repositoryItemElem: RepositoryCard,
+      repositoryList: null,
       tabs: ['Profile', 'Intelligences', 'Activities', 'Reports', 'Payments'],
+      repositoriesLimit: 3,
     };
   },
   computed: {
@@ -57,14 +88,34 @@ export default {
       'myProfile',
     ]),
   },
+  mounted() {
+    this.updateMyProfile();
+    this.updateMyRepositories();
+  },
+  methods: {
+    ...mapActions([
+      'getMyRepositories',
+      'updateMyProfile',
+    ]),
+    async updateMyRepositories() {
+      this.repositoryList = await this.getMyRepositories(this.repositoriesLimit);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import '~@/assets/scss/colors.scss';
 $shadow-color: #00000029;
+h1 {
+        max-width: 58.25rem;
+        padding: 0 1rem;
+        margin: 0.75rem auto;
+        display: block;
+      }
 
     .profile {
+
         &__avatar {
             width: 90px;
             height: 90px;
@@ -104,6 +155,18 @@ $shadow-color: #00000029;
           &__container {
             background-color: $color-white;
             width: 100%;
+          }
+        }
+
+        &__repositories {
+
+          &__cards {
+            display: flex;
+            flex-wrap: wrap;
+            max-width: 72.875rem;
+            min-height: 31.25rem;
+            padding: 1rem;
+            margin: auto;
           }
         }
     }
