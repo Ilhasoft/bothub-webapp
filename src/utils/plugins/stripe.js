dbc1ca7b601ac01c0fc0ec120068e5f94cd9c72f
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-07-07 17:45:00 -0300

    Changed var name

--- src/utils/plugins/stripe.js
+++ src/utils/plugins/stripe.js
@@ -1,14 +1,14 @@
 export default {
-  addStripe: (callback) => {
+  addStripe: (onFinish) => {
     if (!process.env.BOTHUB_WEBAPP_PAYMENT_ENABLED || document.getElementById('stripe-script')) {
-      callback();
+      onFinish();
       return;
     }
     const script = document.createElement('script');
     script.id = 'stripe-script';
     script.src = 'https://js.stripe.com/v3/';
     script.async = true;
-    script.onload = callback;
+    script.onload = onFinish;
     document.head.appendChild(script);
   },
 };
