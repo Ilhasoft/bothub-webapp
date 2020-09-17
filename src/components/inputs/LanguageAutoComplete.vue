09f8947075d9369595ffc7437d0cdb2725548445
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-09-17 14:56:58 -0300

    Fixed language target language not working

--- src/components/inputs/LanguageAutoComplete.vue
+++ src/components/inputs/LanguageAutoComplete.vue
@@ -1,12 +1,5 @@
 <template>
-  <fetch-choice-input
-    v-if="fetch"
-    :fetch="fetch"
-    :label-placeholder="labelPlaceholder"
-    v-model="value"
-    @input="update()"/>
   <b-autocomplete
-    v-else-if="compact"
     v-model="value"
     :placeholder="labelPlaceholder"
     :custom-formatter="formatter"
@@ -16,46 +9,25 @@
     open-on-focus
     @input="updateInput"
     @select="selectOption"/>
-  <b-select
-    v-else
-    v-model="value"
-    expanded
-    @input="update()">
-    <option
-      v-for="choice in choices"
-      :key="choice.value"
-      :value="choice.value">{{ choice.display_name }}</option>
-  </b-select>
 </template>
 
 <script>
-import { formatters } from '@/utils';
-import FetchChoiceInput from './FetchChoiceInput';
+import { formatters, LANGUAGES } from '@/utils/index';
 
 export default {
-  components: {
-    FetchChoiceInput,
-  },
+  name: 'LanguageAutocomplete',
   props: {
-    choices: {
-      type: Array,
-      default: () => [],
-    },
-    fetch: {
-      type: Function,
-      default: null,
-    },
     initialData: {
       type: [Array, String],
-      default: null,
-    },
-    compact: {
-      type: Boolean,
       default: null,
     },
     labelPlaceholder: {
       type: String,
       default: '',
+    },
+    exclude: {
+      type: Array,
+      default: () => ([]),
     },
   },
   data() {
@@ -65,13 +37,16 @@ export default {
     };
   },
   computed: {
+    choices() {
+      return Object.keys(LANGUAGES)
+        .filter(([lang]) => !this.exclude.includes(lang))
+        .map(lang => ({ value: lang, display_name: LANGUAGES[lang] }));
+    },
     filteredChoices() {
       if (!this.value || this.value.length === 0) { return this.choices; }
-      const search = new RegExp(formatters.bothubItemKey()(this.value.toLowerCase()));
+      const search = new RegExp(formatters.bothubItemKey()(this.value));
       return this.choices
-        .filter(
-          choice => search.test(formatters.bothubItemKey()(choice.display_name.toLowerCase())),
-        );
+        .filter(choice => search.test(formatters.bothubItemKey()(choice.display_name)));
     },
   },
   mounted() {
@@ -79,9 +54,9 @@ export default {
   },
   methods: {
     updateInput() {
-      const search = formatters.bothubItemKey()(this.value.toLowerCase());
+      const search = formatters.bothubItemKey()(this.value);
       const option = this.choices.find(
-        choice => formatters.bothubItemKey()(choice.display_name.toLowerCase())
+        choice => formatters.bothubItemKey()(choice.display_name)
                     === search,
       );
       if (option) {
