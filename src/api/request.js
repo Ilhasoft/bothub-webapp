fe68328175e36781d2a0286ab1a48b11f49adce8
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-09-25 11:40:40 -0300

    Added sentry log for timeout

--- src/api/request.js
+++ src/api/request.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
+import * as Sentry from '@sentry/browser';
 import store from '../store';
 
 export default {
   get $http() {
-    return axios.create({
+    const client = axios.create({
       baseURL: process.env.API_BASE_URL,
       headers: {
         ...(store.getters.authenticated
           ? { Authorization: `Token ${store.getters.authToken}` } : {}),
       },
     });
+    client.interceptors.response.use(
+      res => res,
+      (err) => {
+        if (err.response.status === 500 || err.response.status === 408) {
+          Sentry.captureException(err);
+        }
+        throw err;
+      },
+    );
+    return client;
   },
 };
