55d142499703961cd2a996a97753aab54d6c5c44
Alexandre <alexandre.azevedo@ilhasoft.com.br>
2020-10-05 12:14:58 -0300

    Added permission

--- src/views/repository/Translate.vue
+++ src/views/repository/Translate.vue
@@ -51,7 +51,7 @@
             <p>{{ $t('webapp.translate.subtitle_translate') }}</p>
           </div>
           <auto-translate
-            v-if="repository"
+            v-if="repository && repository.authorization.can_translate"
             :translate-to="translate.to"
             :repository-uuid="repository.uuid"
             @onTranslate="translating = true"
