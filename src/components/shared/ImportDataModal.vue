3579bb01c626c3f5a151ecd60eff24bc7bced25a
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-12-01 18:00:15 -0300

    added importDataModal on pages

---
+++ src/components/shared/ImportDataModal.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="import-data-modal">
+    <b-modal
+      :active.sync="isModalVisible"
+      :destroy-on-hide="false"
+      :can-cancel="false"
+      has-modal-card
+      aria-role="dialog"
+      class="import-data-modal__card"
+      aria-modal>
+      <div
+        class="modal-card import-data-modal__modal-style">
+        <header class="modal-card-head import-data-modal__modal-style__header">
+          <p>Importar Dataset</p>
+        </header>
+        <section class="modal-card-body">
+          <b-field
+            class="import-data-modal__custom-file-upload">
+            <div class="import-data-modal__custom-file-upload__input">
+              <b-upload v-model="selectedFile">
+                <a class="button import-data-modal__custom-file-upload__input__button">
+                  <b-icon
+                    icon="upload"
+                    type="is-white"/>
+                </a>
+              </b-upload>
+              <div
+                v-if="selectedFile"
+                class="import-data-modal__custom-file-upload__input__file">
+                {{ selectedFile.name }}
+                <div
+                  class="import-data-modal__custom-file-upload__input__icon"
+                  @click="removeSelectedFile">
+                  <b-icon
+                    icon="close-circle"
+                    custom-size="mdi-18px"
+                  />
+                </div>
+              </div>
+              <div
+                v-else
+                class="import-data-modal__custom-file-upload__input__file">
+                <span>Nenhum arquivo selecionado</span>
+              </div>
+            </div>
+            <p>{{ errorMessage }}</p>
+          </b-field>
+        </section>
+        <footer class="modal-card-foot">
+          <div class="import-data-modal__modal-style__style-button">
+            <b-button
+              class="modal-button"
+              type="is-white"
+              @click="dispatchCloseImportModal()">
+              {{ $t('webapp.translate.import_button_cancel') }}
+            </b-button>
+            <b-button
+              :loading="isButtonLoading"
+              :disabled="isImportButtonVisible"
+              class="modal-button"
+              type="is-primary"
+              @click="dispatchImport()">
+              {{ $t('webapp.translate.import_button') }}
+            </b-button>
+          </div>
+        </footer>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImportDataModal',
+  props: {
+    isModalVisible: {
+      type: Boolean,
+      default: false,
+    },
+    isImportButtonVisible: {
+      type: Boolean,
+      default: false,
+    },
+    isButtonLoading: {
+      type: Boolean,
+      default: false,
+    },
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      selectedFile: null,
+    };
+  },
+  watch: {
+    selectedFile() {
+      if (this.selectedFile !== null) {
+        this.$emit('translationFileChanged', this.selectedFile);
+      }
+    },
+  },
+  methods: {
+    removeSelectedFile() {
+      this.selectedFile = null;
+    },
+    dispatchImport() {
+      this.selectedFile = null;
+      this.$emit('dispatchImportFile');
+    },
+    dispatchCloseImportModal() {
+      this.selectedFile = null;
+      this.$emit('dispatchCloseModal');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/colors.scss';
+@import '~@/assets/scss/variables.scss';
+
+.import-data-modal {
+
+  &__modal-style{
+      width: 26.5rem;
+      @media (max-width: $mobile-width) {
+        padding-left: 5rem
+      }
+    &__header{
+      p{
+        font-size: 1.5rem;
+      }
+    }
+
+    &__style-button{
+      width:100%;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+
+      .modal-button {
+        cursor: pointer;
+        border: 1px solid #dbdbdb;
+        text-align: center;
+        border-radius: 4px;
+        font-family: $font-family;
+        box-shadow: none;
+
+        &:hover{
+          border: 1px solid #c2c2c2;
+        }
+      }
+    }
+  }
+    &__custom-file-upload {
+    display: flex;
+    flex-direction: column;
+
+    &__input{
+    display: flex;
+    width: 100%;
+
+    &__button{
+      background-color: #9E9E9E;
+      padding: 0 2rem;
+    }
+
+    &__file{
+      border: 1px solid #D5D5D5;
+      color: #BABABA;
+      font-size: $font-size;
+      font-family: $font-family;
+      display: flex;
+      width: 100%;
+      justify-content:center;
+      align-items:center;
+      border-top-right-radius: 0.4rem;
+      border-bottom-right-radius: 0.4rem;
+
+      span{
+        font-size: 1rem;
+      }
+    }
+    &__icon{
+      cursor: pointer;
+      color:#D5D5D5;
+      display:flex;
+      align-items: center;
+      &:hover{
+        color: $color-grey-dark;
+      }
+    }
+    }
+  }
+}
+
+</style>
--- src/components/shared/ImportIntelligence.vue
+++ src/components/shared/ImportIntelligence.vue
@@ -1,46 +1,91 @@
 <template>
   <div class="import-intelligence">
-    <p>Importe sua inteligência</p>
-    <p class="import-intelligence__description">
+    <h2
+      v-if="isTrainingPage"
+      class="import-intelligence__title">Importe sua inteligência</h2>
+    <p v-else>Importe sua inteligência</p>
+    <p
+      v-if="!isTrainingPage"
+      class="import-intelligence__description">
       Caso você já possua uma inteligência de outra plataforma
       que gostaria de usá-la aqui, basta selecionar o arquivo
       correspondente aos modelos do Rasa, Wit.AI ou Bothub,
       que automaticamente os dados serão reconhecidos
     </p>
+    <p
+      v-else
+      class="import-intelligence__description-train">
+      Para importar, basta selecionar o arquivo correspondente
+      aos modelos Rasa, Wit.AI ou Bothub, e os dados serão automaticamente
+      reconhecidos.
+    </p>
     <div class="import-intelligence__content">
       <b-button
+        v-if="intelligenceFile === null"
         type="is-primary"
-        class="import-intelligence__content__import-button">
+        class="import-intelligence__content__import-button"
+        @click="setVisibleImportModal()">
         Importar dados
       </b-button>
-      <b-field class="import-intelligence__content__input">
+      <b-field
+        v-else
+        class="import-intelligence__content__input">
         <b-input
-          v-model="email"
-          :placeholder="fileUploaded"
+          :placeholder="intelligenceFile.name"
           icon="paperclip"
           icon-right="close"
           disabled
           icon-right-clickable
-          @icon-right-click="clearIconClick"/>
+          @icon-right-click="clearImportedFile()"/>
       </b-field>
-
     </div>
+
+    <import-data-modal
+      :is-modal-visible="importModalVisible"
+      @translationFileChanged="intelligenceFile = $event"
+      @dispatchCloseModal="closeImportModal()"
+      @dispatchImportFile="importSelectedFile()"/>
   </div>
 </template>
 
 <script>
+import ImportDataModal from './ImportDataModal';
+
 export default {
   name: 'ImportIntelligence',
+  components: { ImportDataModal },
   props: {
     fileUploaded: {
       type: String,
       default: 'Wit.AI',
     },
+    isTrainingPage: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
-
+      intelligenceFile: null,
+      importModalVisible: false,
     };
+  },
+  methods: {
+    importSelectedFile() {
+      this.importModalVisible = false;
+    },
+    setVisibleImportModal() {
+      this.importModalVisible = true;
+    },
+    closeImportModal() {
+      this.importModalVisible = false;
+      this.intelligenceFile = null;
+    },
+    clearImportedFile() {
+      this.$nextTick(() => {
+        this.intelligenceFile = null;
+      });
+    },
   },
 };
 </script>
@@ -54,12 +99,24 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: flex-start;
-    margin-top: -1rem;
+    margin-top: -2rem;
     width: 100%;
+
+    &__title{
+        margin-top: 2.2rem;
+    }
 
     &__description {
         text-align: justify;
-        font-size: 0.75rem;
+        font-size: 12px;
+        @media (max-width: $mobile-width) {
+            max-width: 80%;
+        }
+    }
+
+    &__description-train {
+        text-align: justify;
+        max-width: 80%;
     }
 
     &__content {
@@ -76,13 +133,13 @@ export default {
             font-family: $font-family;
             box-shadow: 0px 3px 6px #00000029;
             border-radius: 6px;
+
         }
 
         &__input {
-                width: 12rem;
-                border: 1px solid $color-grey;
-                margin-left: 2rem;
-                border-radius: 4px;
+            width: 14rem;
+            border: 1px solid $color-grey;
+            border-radius: 4px;
         }
     }
     @media (max-width: $mobile-width) {
