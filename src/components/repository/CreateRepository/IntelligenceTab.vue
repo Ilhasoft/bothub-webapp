da755cf43537ad63eb4a4b33f958b0df8e66ca19
lucia_vulcano <vulcanolucia@gmail.com>
2021-09-23 13:39:56 -0300

    enable new content inteligence

--- src/components/repository/CreateRepository/IntelligenceTab.vue
+++ src/components/repository/CreateRepository/IntelligenceTab.vue
@@ -26,14 +26,14 @@
           @click.native="intelligence.repository_type = 'classifier'"
         />
         <unnnic-card
-          :clickable = false
+          clickable
           :title="$t('webapp.create_repository.intelligence_type_content_title')"
           :description="$t('webapp.create_repository.intelligence_type_content_description')"
           type="content"
           icon="lock-2-1"
           class="create-repository__intelligence__type__content"
-          :enabled="false"
-          @click.native="false"
+          :enabled="intelligence.repository_type === 'content'"
+          @click.native="intelligence.repository_type = 'content'"
         />
       </div>
       <div class="create-repository__intelligence__buttons">
